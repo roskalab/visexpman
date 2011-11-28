172efbbef2b56ed0fd55b6cf345631ded68d709e
Daniel Hillier <hillier_dani@yahoo.fr>
2011-11-28 15:35:19 +0100

    storing position number with shown_directions

--- users/daniel/moving_dot.py
+++ users/daniel/moving_dot.py
@@ -345,7 +345,7 @@ class MovingDot(experiment.Experiment):
                     for n in range(self.experiment_config.NDOTS):
                         coords.append(arow_col[cai][b][n][:,f])
                     self.row_col[-1].extend(coords)
-                self.shown_directions[-1].append(allangles[a1]) # at each coordinate we store the direction, thus we won't need to analyze dot coordinates 
+                self.shown_directions[-1].append((allangles[a1], sum(len(s1) for s1 in self.row_col[-1]))) # at each coordinate we store the direction, thus we won't need to analyze dot coordinates 
                 self.line_end[-1].append(arow_col[cai][b][0].shape[1])
             self.row_col[-1]=utils.rc(numpy.array(self.row_col[-1]))
         pass
@@ -469,10 +469,10 @@ class MovingDotTestConfig(experiment.ExperimentConfig):
     def _create_application_parameters(self):  
     	self.MAX_FRAGMENT_TIME = 120.0
         self.DIAMETER_UM = [200]
-        self.ANGLES = [0] # degrees
+        self.ANGLES = [0, 90, 180, 270] # degrees
         self.SPEED = [1200] #[40deg/s] % deg/s should not be larger than screen size
         self.AMPLITUDE = 0.5
-        self.REPEATS = 1
+        self.REPEATS = 2
         self.PDURATION = 0
         self.GRIDSTEP = 1.0/1 # how much to step the dot's position between each sweep (GRIDSTEP*diameter)
         self.NDOTS = 1
