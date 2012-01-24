ec03c42c783564a22c19fd962724fdb56d9ccc65
Daniel Hillier <daniel.hillier@fmi.ch>
2012-01-24 15:44:45 +0100

    added method that imports code from a string

--- engine/generic/introspect.py
+++ engine/generic/introspect.py
@@ -253,7 +253,40 @@ class ModifiableIterator(object):
         self.consuming.extend(items)
         self.list = deque([f for f in self.list if f not in items])
     
-        
+      ## {{{ http://code.activestate.com/recipes/82234/ (r1)
+# Importing a dynamically generated module
+
+def import_code(code,name,add_to_sys_modules=0):
+    """
+    Import dynamically generated code as a module. code is the
+    object containing the code (a string, a file handle or an
+    actual compiled code object, same types as accepted by an
+    exec statement). The name is the name to give to the module,
+    and the final argument says wheter to add it to sys.modules
+    or not. If it is added, a subsequent import statement using
+    name will return this module. If it is not added to sys.modules
+    import will try to load it in the normal fashion.
+
+    import foo
+
+    is equivalent to
+
+    foofile = open("/path/to/foo.py")
+    foo = importCode(foofile,"foo",1)
+
+    Returns a newly generated module.
+    """
+    import sys,imp
+
+    module = imp.new_module(name)
+
+    exec code in module.__dict__
+    if add_to_sys_modules:
+        sys.modules[name] = module
+    return module
+
+
+  
 import unittest
 class TestUtils(unittest.TestCase):
     def setUp(self):
@@ -272,6 +305,23 @@ class TestUtils(unittest.TestCase):
             result.append(item)
         self.assertEqual(result,[1,2,1,3,4,5])
         
+    def test_dynamic_import(self):
+        # Example
+        code = \
+        """
+        def testFunc():
+            print "spam!"
+
+        class testClass:
+            def testMethod(self):
+                print "eggs!"
+        """
+
+        m = import_code(code,"test")
+        m.testFunc()
+        o = m.testClass()
+        o.testMethod()
+        ## end of http://code.activestate.com/recipes/82234/ }}}
     
 if __name__=='__main__':
     unittest.main()
