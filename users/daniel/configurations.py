db467a69a84bfbe02b007540da3ac5d4616f897f
Zoltan Raics <rz@rlvivo1.fmi.ch>
2013-05-21 16:54:34 +0200

    notebook config update

--- users/daniel/configurations.py
+++ users/daniel/configurations.py
@@ -177,120 +177,6 @@ class MBP(VisionExperimentConfig):
         VisionExperimentConfig._create_parameters_from_locals(self, locals())
         #VisionExperimentConfig._set_parameters_from_locals(self, locals())
         
-class DebugOnLaptop(RcCorticalCaImagingConfig):
-    '''
-    Windows development machine
-    '''
-    def _set_user_parameters(self):        
-        EXPERIMENT_CONFIG = 'MovingRectangleConfig'
-#        EXPERIMENT_CONFIG = 'Dummy'
-        #=== paths/data handling ===
-        use_drive = 'c'
-        if os.name == 'nt':
-            if use_drive == 'g':
-                root_folder = 'g:\\User\\Zoltan'
-            elif use_drive =='v':
-                root_folder = 'V:\\'
-            elif use_drive =='c':
-                root_folder = 'c:\\visexp'
-        else:
-            root_folder = '/home/zoltan/visexp/' 
-        drive_data_folder = os.path.join(root_folder, 'experiment_data')
-        LOG_PATH = os.path.join(drive_data_folder, 'log')
-        EXPERIMENT_LOG_PATH = LOG_PATH        
-        EXPERIMENT_DATA_PATH = drive_data_folder
-        if use_drive == 'g':
-            MES_DATA_FOLDER = 'g:\\User\\Zoltan\\data'
-        elif use_drive =='v':
-            MES_DATA_FOLDER = 'V:\\debug\\data'
-        elif use_drive =='c':
-            MES_DATA_FOLDER = 'c:\\visexp\\debug\\data'
-        self.CONTEXT_NAME = 'gui_dev.hdf5'
-        CONTEXT_PATH = os.path.join(root_folder, 'context')
-        CAPTURE_PATH = os.path.join(drive_data_folder, 'capture')
-        EXPERIMENT_FILE_FORMAT = 'hdf5'
-        #Create folders that does not exists
-        for folder in [LOG_PATH, EXPERIMENT_DATA_PATH, EXPERIMENT_LOG_PATH, MES_DATA_FOLDER, CONTEXT_PATH, CAPTURE_PATH]:
-            file.mkdir_notexists(folder)
-        
-        #=== screen ===
-        FULLSCREEN = not True
-        SCREEN_RESOLUTION = utils.cr([800, 600])
-        COORDINATE_SYSTEM='ulcorner'
-        ENABLE_FRAME_CAPTURE =  False
-        SCREEN_EXPECTED_FRAME_RATE = 60.0
-        SCREEN_MAX_FRAME_RATE = 60.0        
-        
-        #=== experiment specific ===
-        IMAGE_PROJECTED_ON_RETINA = False
-        SCREEN_DISTANCE_FROM_MOUSE_EYE = [280.0, [0, 300]] #mm
-        SCREEN_PIXEL_WIDTH = [0.56, [0, 0.99]] # mm, must be measured by hand (depends on how far the projector is from the screen)
-        degrees = 10.0*1/300 # 300 um on the retina corresponds to 10 visual degrees.  
-        SCREEN_UM_TO_PIXEL_SCALE = numpy.tan(numpy.pi/180*degrees)*SCREEN_DISTANCE_FROM_MOUSE_EYE[0]/SCREEN_PIXEL_WIDTH[0] #1 um on the retina is this many pixels on the screen        
-        MAXIMUM_RECORDING_DURATION = [1000, [0, 10000]] #100
-        MES_TIMEOUT = 10.0
-        PLATFORM = 'standalone'
-#        PLATFORM = 'mes'
-        
-        #=== Network ===
-        self.COMMAND_RELAY_SERVER['RELAY_SERVER_IP'] = 'localhost'
-        self.COMMAND_RELAY_SERVER['CLIENTS_ENABLE'] = True
-        #=== hardware ===
-        ENABLE_PARALLEL_PORT = False
-        ACQUISITION_TRIGGER_PIN = 2
-        FRAME_TRIGGER_PIN = 0
-        FRAME_TRIGGER_PULSE_WIDTH = 1e-3
-        
-        #=== stage ===
-        motor_serial_port = {
-                                    'port' :  'COM1',
-                                    'baudrate' : 19200,
-                                    'parity' : serial.PARITY_NONE,
-                                    'stopbits' : serial.STOPBITS_ONE,
-                                    'bytesize' : serial.EIGHTBITS,                    
-                                    }
-                                    
-        STAGE = [{'SERIAL_PORT' : motor_serial_port,
-                 'ENABLE': False,
-                 'SPEED': 800,
-                 'ACCELERATION' : 200,
-                 'MOVE_TIMEOUT' : 45.0,
-                 'UM_PER_USTEP' : (1.0/51.0)*numpy.ones(3, dtype = numpy.float)
-                 }]
-                 
-                                                
-        #=== DAQ ===
-        STIM_SYNC_CHANNEL_INDEX = 1
-        MES_SYNC_CHANNEL_INDEX = 0
-        DAQ_CONFIG = [
-                    {
-                    'ANALOG_CONFIG' : 'ai', #'ai', 'ao', 'aio', 'undefined'
-                    'DAQ_TIMEOUT' : 3.0,
-                    'SAMPLE_RATE' : 5000,
-                    'AI_CHANNEL' : 'Dev1/ai0:2',
-                    'MAX_VOLTAGE' : 10.0,
-                    'MIN_VOLTAGE' : -10.0,
-                    'DURATION_OF_AI_READ' : 2*MAXIMUM_RECORDING_DURATION[0],
-                    'ENABLE' : False
-                    },
-                    {
-                    'ANALOG_CONFIG' : 'ao', #'ai', 'ao', 'aio', 'undefined'
-                    'DAQ_TIMEOUT' : 3.0,
-                    'SAMPLE_RATE' : 1000,
-                    'AO_CHANNEL' : 'Dev1/ao0',
-                    'MAX_VOLTAGE' : 10.0,
-                    'MIN_VOLTAGE' : 0.0,
-                    'ENABLE' : False
-                    }
-                    ]
-        
-        #=== Others ===
-        
-        USER_EXPERIMENT_COMMANDS = {'stop': {'key': 's', 'domain': ['running experiment']}, 
-                                    'next': {'key': 'n', 'domain': ['running experiment']},}
-        
-        self._create_parameters_from_locals(locals())
-        
 class RcMicroscopeSetup(RcCorticalCaImagingConfig):
     '''
     Visual stimulation machine of 3D microscope setup
@@ -433,6 +319,83 @@ class RcMicroscopeSetup(RcCorticalCaImagingConfig):
         self.SHOW_PREVIEW_WINDOW=True
         self._create_parameters_from_locals(locals(), check_path = check_path)
         
+class DebugOnLaptop(RcMicroscopeSetup):
+    '''
+    Windows development machine
+    '''
+    def _set_user_parameters(self):
+        RcMicroscopeSetup._set_user_parameters(self)
+        EXPERIMENT_CONFIG = 'MovingRectangleConfig'
+#        EXPERIMENT_CONFIG = 'Dummy'
+        #=== paths/data handling ===
+        use_drive = 'c'
+        if os.name == 'nt':
+            if use_drive == 'g':
+                root_folder = 'g:\\User\\Zoltan'
+            elif use_drive =='v':
+                root_folder = 'V:\\'
+            elif use_drive =='c':
+                root_folder = 'c:\\visexp'
+        else:
+            root_folder = '/home/zoltan/visexp/' 
+        drive_data_folder = os.path.join(root_folder, 'experiment_data')
+        LOG_PATH = os.path.join(drive_data_folder, 'log')
+        EXPERIMENT_LOG_PATH = LOG_PATH        
+        EXPERIMENT_DATA_PATH = drive_data_folder
+        if use_drive == 'g':
+            MES_DATA_FOLDER = 'g:\\User\\Zoltan\\data'
+        elif use_drive =='v':
+            MES_DATA_FOLDER = 'V:\\debug\\data'
+        elif use_drive =='c':
+            MES_DATA_FOLDER = 'c:\\visexp\\debug\\data'
+        self.CONTEXT_NAME = 'gui_dev.hdf5'
+        CONTEXT_PATH = os.path.join(root_folder, 'context')
+        CAPTURE_PATH = os.path.join(drive_data_folder, 'capture')
+        EXPERIMENT_FILE_FORMAT = 'hdf5'
+        #Create folders that does not exists
+        for folder in [LOG_PATH, EXPERIMENT_DATA_PATH, EXPERIMENT_LOG_PATH, MES_DATA_FOLDER, CONTEXT_PATH, CAPTURE_PATH]:
+            file.mkdir_notexists(folder)
+        
+        #=== screen ===
+        FULLSCREEN = True
+        SCREEN_RESOLUTION = utils.cr([800, 600])
+        ENABLE_FRAME_CAPTURE =  False
+        COORDINATE_SYSTEM='ulcorner'
+        PLATFORM = 'standalone'
+
+        #=== Network ===
+        self.COMMAND_RELAY_SERVER['RELAY_SERVER_IP'] = 'localhost'
+        self.COMMAND_RELAY_SERVER['CLIENTS_ENABLE'] = True
+        #=== hardware ===
+        ENABLE_PARALLEL_PORT = False
+        ACQUISITION_TRIGGER_PIN = 2
+        FRAME_TRIGGER_PIN = 0
+        FRAME_TRIGGER_PULSE_WIDTH = 1e-3
+        
+        #=== stage ===
+                                    
+        STAGE = [{'ENABLE': False}]
+                 
+                                                
+        #=== DAQ ===
+        STIM_SYNC_CHANNEL_INDEX = 1
+        MES_SYNC_CHANNEL_INDEX = 0
+        DAQ_CONFIG = [
+                    {
+                    'ENABLE' : False
+                    },
+                    {
+                    'ENABLE' : False
+                    }
+                    ]
+        
+        #=== Others ===
+        
+        USER_EXPERIMENT_COMMANDS = {'stop': {'key': 's', 'domain': ['running experiment']}, 
+                                    'next': {'key': 'n', 'domain': ['running experiment']},}
+        
+        self._create_parameters_from_locals(locals())
+
 class DebugRlvivo1(RcMicroscopeSetup):
     '''
     Rlvivo1 development
