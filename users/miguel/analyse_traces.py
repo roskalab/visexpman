cd87f5d7a8e01bcc2fd5edaa04c855ca7deeaaaa
Zoltan Raics <zoltan@bhf415.(none)>
2013-05-27 18:03:52 +0200

    elphys data analyzer (not ready)

---
+++ users/miguel/analyse_traces.py
@@ -0,0 +1,23 @@
+from visexpman.engine.vision_experiment import experiment_data
+import scipy.io
+from matplotlib.pyplot import plot, show,figure,legend
+import numpy
+
+if __name__=='__main__':
+
+
+
+
+    pmat = '/home/zoltan/Downloads/20130315_C2#015Miguel_Ch2!spike0_00000_0.mat'
+    pphys = '/home/zoltan/Downloads/20130315_C2#015Miguel_Ch2!spike0.phys'
+    raw_elphys_signal, metadata = experiment_data.read_phys(pphys)
+    fs = float(metadata['Sample Rate'])
+    prerecord_time =   float(metadata['Pre-Record Time (mS)'])/1000.0
+    voltage_scale =   float(metadata['Waveform Scale Factors'])
+    elphys_signal = raw_elphys_signal[0,:]*voltage_scale
+    
+    stim_data = scipy.io.loadmat(pmat, mat_dtype=True)
+    stim_data['stimulus_frame_info']
+    plot(elphys_signal[::10])
+    show()
+
