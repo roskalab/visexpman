aaeab09dd33de702462c087750c283d64f1b7e16
Zoltan Raics <rz@rlvivo1.fmi.ch>
2013-05-28 13:49:18 +0200

    minor change

--- users/miguel/analyse_traces.py
+++ users/miguel/analyse_traces.py
@@ -2,14 +2,16 @@ from visexpman.engine.vision_experiment import experiment_data
 import scipy.io
 from matplotlib.pyplot import plot, show,figure,legend
 import numpy
+import os.path
 
 if __name__=='__main__':
 
 
 
-
-    pmat = '/home/zoltan/Downloads/20130315_C2#015Miguel_Ch2!spike0_00000_0.mat'
-    pphys = '/home/zoltan/Downloads/20130315_C2#015Miguel_Ch2!spike0.phys'
+    folder = '/mnt/datafast/'
+    folder = 'V:\\'
+    pmat = os.path.join(folder, 'debug',  'migueldata',  '20130315_C2#015Miguel_Ch2!spike0_00000_0.mat')
+    pphys = os.path.join(folder, 'debug',  'migueldata',  '20130315_C2#015Miguel_Ch2!spike0.phys')
     raw_elphys_signal, metadata = experiment_data.read_phys(pphys)
     fs = float(metadata['Sample Rate'])
     prerecord_time =   float(metadata['Pre-Record Time (mS)'])/1000.0
