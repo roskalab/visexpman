29322e063d95b784d45ac6c87c92b299460be8b0
rzoli <rz@rzws1.fmi.ch>
2014-10-09 17:10:19 +0200

    setup config added

--- users/common/elphys_setups.py
+++ users/common/elphys_setups.py
@@ -2,6 +2,7 @@ import os
 import os.path
 import numpy
 import tempfile
+import serial
 from visexpman.engine.generic import utils,fileop
 from visexpman.engine.vision_experiment.configuration import ElphysRetinalCaImagingConfig
 
@@ -50,3 +51,52 @@ class ReiSetupConfig(ElphysRetinalCaImagingConfig):
         self.SCANNER_CHARACTERISTICS['PHASE'] = \
                     [9.50324884e-08,  -1.43226725e-07, 1.50117389e-05,  -1.41414186e-04,   5.90072950e-04,   5.40402050e-03,  -1.18021600e-02]#(p1*a+p2)*f**2+(p3*a**2+p4*a+p5)*f+(p6*a+p7), in radians
         self._create_parameters_from_locals(locals())
+
+class TamasSetupConfig(ElphysConfig):
+    '''
+    '''
+    def _set_user_parameters(self):
+        root_folder = 'c:\\Data'
+        LOG_PATH = os.path.join(root_folder, 'log')
+        if not os.path.exists(LOG_PATH):
+            os.mkdir(LOG_PATH)
+        DATA_PATH = root_folder
+        EXPERIMENT_DATA_PATH = root_folder
+        EXPERIMENT_LOG_PATH = LOG_PATH
+        
+        #=== screen ===
+        FULLSCREEN = True
+        SCREEN_RESOLUTION = utils.cr([1024,768])
+        COORDINATE_SYSTEM='center'
+        
+        ENABLE_FRAME_CAPTURE = False
+        INSERT_FLIP_DELAY = True
+        SCREEN_EXPECTED_FRAME_RATE = 60.0
+        SCREEN_MAX_FRAME_RATE = 60.0
+        SCREEN_UM_TO_PIXEL_SCALE = 3.0
+        #SCREEN_PIXEL_WIDTH = 0.5#mm 200 pixels = 100 mm
+        
+        #=== hardware ===
+        ENABLE_PARALLEL_PORT = (os.name == 'nt')
+        ACQUISITION_TRIGGER_PIN = 2
+        BLOCK_TRIGGER_PIN = 0
+        FRAME_TRIGGER_PIN = 1
+        ENABLE_UDP = True
+        
+        ENABLE_FILTERWHEEL=True
+        FILTERWHEEL_SERIAL_PORT = [{
+                                    'port' :  'COM1',
+                                    'baudrate' : 9600,
+                                    'parity' : serial.PARITY_NONE,
+                                    'stopbits' : serial.STOPBITS_ONE,
+                                    'bytesize' : serial.EIGHTBITS,                                    
+                                    },
+                        {
+                                    'port' :  'COM3',
+                                    'baudrate' : 9600,
+                                    'parity' : serial.PARITY_NONE,
+                                    'stopbits' : serial.STOPBITS_ONE,
+                                    'bytesize' : serial.EIGHTBITS,                                    
+                                    },
+                        ]
+
