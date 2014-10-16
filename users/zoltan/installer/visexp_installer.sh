6b25e400f7ee0416ae5f5d63b5b62f0300de4a5d
rzoli <rz@rzws1.fmi.ch>
2014-10-16 23:42:16 +0200

    linux installer update

--- users/zoltan/installer/visexp_installer.sh
+++ users/zoltan/installer/visexp_installer.sh
@@ -3,7 +3,11 @@
 apt-get install gnome-commander eric git synaptic libav-tools libavcodec-extra-53
 #TODO: update fstab, install smb, mount rlvivo1, mdrive, gdrive
 #visexp specific
-apt-get install python-numpy python-scipy python-zc.lockfile python-sip python-imaging python-tables python-serial python-parallel python-qt4 python-qwt5-qt4 python-opengl python-pygame python-sklearn python wxgtk2.8 python-opencv python-matplotlib python-zmq python-celery python-pp libhdf5-serial-dev
+apt-get install python-numpy python-scipy python-zc.lockfile python-sip python-imaging python-serial python-parallel python-qt4 python-qwt5-qt4 python-opengl python-pygame python-sklearn python wxgtk2.8 python-opencv python-matplotlib python-zmq python-celery python-pp libhdf5-serial-dev
+apt-get install python pip
+pip install numexpr
+pip install Cython
+pip install tables
 mkdir visexp
 cd visexp
 git clone git@github.com:roskalab/visexpman.git
