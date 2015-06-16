74e6d5abd9bbe0d7eb7b31301416ab8d1e68549b
Roland Diggelmann <roland.diggelmann@bsse.ethz.ch>
2015-06-16 18:06:13 +0200

    install script bug fix

--- install_libraries.sh
+++ install_libraries.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo apt-get python-psutil python-numpy python-scipy python-qwt5-qt4 python-zc.lockfile python-tables python-pygame
+sudo apt-get install python-psutil python-numpy python-scipy python-qwt5-qt4 python-zc.lockfile python-tables python-pygame
 
 
 # In order to use the parallel port, change the driver:
