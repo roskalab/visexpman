1cf22a494d7fb8fe90ff9c00c2c2de928519e2e9
rzoli <zoltan.raics@raicszoltan.hu>
2015-06-18 18:00:27 +0200

    cell detection for non-circular cells

--- users/rajib/ca_analysis.py
+++ users/rajib/ca_analysis.py
@@ -1,9 +1,11 @@
+from PIL import Image
 import numpy
 import tifffile
 from visexpman.engine.vision_experiment import cone_data
 import scipy.ndimage.interpolation
+import scipy.ndimage.filters
 from pylab import *
-from visexpman.engine.generic import fileop, signal,stringop,utils,introspect
+from visexpman.engine.generic import fileop, signal,stringop,utils,introspect,geometry
 #Questions to Rajib: image scale
 #TODO: background !!!!!!
 
@@ -24,11 +26,12 @@ def file2cells(filename):
     from scipy.ndimage.filters import gaussian_filter
     from visexpA.engine.dataprocessors import signal as signal2
     centers = signal2.getextrema(gaussian_filter(meanimage,25), method = 'regmax')
-    maxcellsize=300
+    maxcellsize=100
     roisonimgr=numpy.zeros_like(meanimage)
     roisonimgb=numpy.zeros_like(meanimage)
     somarois = []
     for c in centers:
+        if c['row']!=506: continue
         corner = [c['row']-0.5*maxcellsize,c['row']+0.5*maxcellsize,c['col']-0.5*maxcellsize,c['col']+0.5*maxcellsize]
         for i in range(4):
             if corner[i]<0:
@@ -36,18 +39,54 @@ def file2cells(filename):
             if corner[i]>meanimage.shape[i/2%2]:
                 corner[i]=meanimage.shape[i/2%2]-1
         r=meanimage[corner[0]:corner[1],corner[2]:corner[3]]
-        img=gaussian_filter(r,4)
+        img=gaussian_filter(r,4)#remove scanning aritfact
         labeled, nsegments = scipy.ndimage.measurements.label(numpy.where(img>threshold_otsu(img),1,0))
         roi_pixels_w = utils.rc(numpy.where(labeled ==labeled[c['row']-corner[0]-1, c['col']-corner[2]-1]))
         roi_pixels = numpy.copy(roi_pixels_w)
         roi_pixels['row'] = roi_pixels_w['row'] + corner[0]
         roi_pixels['col'] = roi_pixels_w['col'] + corner[2]
+        
+        #Check profile (curve between edge and center
+        mask=geometry.circle_mask([c['row'],c['col']],maxcellsize,meanimage.shape)
+        masked = mask*gaussian_filter(meanimage,2)#Remove scanning artifact
+        nbsum=scipy.ndimage.filters.generic_filter(mask,sum,3)
+        indexes=numpy.nonzero(numpy.where(numpy.logical_and(nbsum<9,nbsum>0),1,0))
+        profile = []
+        contour_pix=[]
+        ds=[]
+        for line in range(0,indexes[0].shape[0],indexes[0].shape[0]/36):
+            endx=indexes[0][line]
+            endy=indexes[1][line]
+            xline=numpy.cast['int'](numpy.round(numpy.linspace(c['row'],endx,maxcellsize),0))
+            yline=numpy.cast['int'](numpy.round(numpy.linspace(c['col'],endy,maxcellsize),0))
+            profile.append(meanimage[xline,yline])
+            d=numpy.diff(gaussian_filter(profile[-1],0.1*maxcellsize)).argmin()
+            ds.append(d)
+            contour_pix.append([xline[d],yline[d]])
+        pass
+        filled = []
+        for p in range(len(contour_pix)-1):
+            npix=numpy.round(numpy.sqrt((numpy.cast['float'](numpy.array(contour_pix[p])-numpy.array(contour_pix[p+1]))**2).sum()))
+            filled.extend(zip(numpy.linspace(contour_pix[p][0],contour_pix[p+1][0],npix+1),numpy.linspace(contour_pix[p][1],contour_pix[p+1][1],npix+1)))
+        contour_pix=filled
+        #TODO: 1 Connect contour points
+        #TODO: 2 Fill object
+#        for p in profile:
+#            numpy.diff(gaussian_filter(p,maxcellsize)).argmin()
+        
+        i=numpy.zeros((masked.shape[0], masked.shape[1], 3))
+        i[:,:,1]=signal.scale(masked)
+        coo=numpy.cast['int'](numpy.array(contour_pix).T)
+        i[coo[0],coo[1],0]=1
+        i=numpy.cast['uint8'](255*i)
+        Image.fromarray(i).save('/tmp/1/c_{0}_{1}.png'.format(c['row'],c['col']))
+#        figure(1);imshow(i);show()#figure(2);[plot(profile[i*350+170]) for i in range(4)];figure(3);[plot(numpy.diff(gaussian_filter(profile[i*350+170],0.1*maxcellsize))) for i in range(4)];show()
         #Check for further segments within roi
         
         tmp=numpy.zeros_like(r)+1
         tmp[roi_pixels_w['row'],roi_pixels_w['col']]=0
         rr=numpy.copy(r)
-        rr[numpy.nonzero(tmp)[0],numpy.nonzero(tmp)[1]]=r[roi_pixels_w['row'],roi_pixels_w['col']].mean()*0
+        rr[numpy.nonzero(tmp)[0],numpy.nonzero(tmp)[1]]=0
         if img.shape[0]*img.shape[1]*0.25>roi_pixels.shape[0]:
             
             
@@ -59,8 +98,8 @@ def file2cells(filename):
             ime=numpy.zeros((meanimage.shape[0],meanimage.shape[1],3),dtype=numpy.uint8)
             ime[corner[0]:corner[1],corner[2]:corner[3],1]=numpy.cast['uint8'](255*signal.scale(r,0.0,1.0))
             ime[numpy.cast['int'](roi_pixels['row']),numpy.cast['int'](roi_pixels['col'] ),2]=255
-            from PIL import Image
-            Image.fromarray(ime).save('/tmp/1/c_{0}_{1}.png'.format(c['row'],c['col']))
+            
+#            Image.fromarray(ime).save('/tmp/1/c_{0}_{1}.png'.format(c['row'],c['col']))
         
 #        else:
 #            roisonimg[c['row']-10:c['row']+10,c['col']-10:c['col']+10]=1
