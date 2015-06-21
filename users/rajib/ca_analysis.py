dbbff8274c38c9e57d3dacfd0f43b71846c9e2ff
rz <zoltan.raics@raicszoltan.hu>
2015-06-21 20:13:12 +0200

    roi curve calculation

--- users/rajib/ca_analysis.py
+++ users/rajib/ca_analysis.py
@@ -1,24 +1,24 @@
+import os
 from PIL import Image
 import numpy
 import tifffile
-from visexpman.engine.vision_experiment import cone_data
+from visexpman.engine.vision_experiment import cone_data,experiment_data
 import scipy.ndimage.interpolation
 import scipy.ndimage.filters
 from pylab import *
 from visexpman.engine.generic import fileop, signal,stringop,utils,introspect,geometry
 #Questions to Rajib: image scale
 #TODO: background !!!!!!
+frame_rate=1
 
-def file2cells(filename):
-    maxcellradius=65
+def file2cells(filename, maxcellradius=65, sigma=0.2):
+    
     image=tifffile.imread(filename)
     meanimage = image.mean(axis=0)
     from skimage.filter import threshold_otsu
     from scipy.ndimage.filters import gaussian_filter
-#    from visexpA.engine.dataprocessors import signal as signal2
-#    centers = signal2.getextrema(gaussian_filter(meanimage,25), method = 'regmax')
     from skimage.feature import peak_local_max
-    centers = utils.rc(peak_local_max(gaussian_filter(meanimage,0.2*maxcellradius),0.2*maxcellradius))
+    centers = utils.rc(peak_local_max(gaussian_filter(meanimage,sigma*maxcellradius),sigma*maxcellradius))
     soma_rois = []
     for c in centers:
         corner = [c['row']-0.5*maxcellradius,c['row']+0.5*maxcellradius,c['col']-0.5*maxcellradius,c['col']+0.5*maxcellradius]
@@ -40,7 +40,8 @@ def file2cells(filename):
         profile = []
         contour_pix=[]
         ds=[]
-        for angle in range(0, 360,15):
+        lines=[]
+        for angle in range(0, 360,20):
             endx=c['row']+numpy.cos(numpy.radians(angle))*maxcellradius
             endy=c['col']+numpy.sin(numpy.radians(angle))*maxcellradius
             if endx<0:
@@ -63,10 +64,15 @@ def file2cells(filename):
                     d=maxcellradius-1
             else:
                 d=d[0]
-            if len(ds)>0:
-                d=(d+ds[-1])/2
             ds.append(d)
+            lines.append([xline,yline])
+        dsf=gaussian_filter(ds,1)
+        for a in range(len(lines)):
+            xline=lines[a][0]
+            yline=lines[a][1]
+            d=dsf[a]
             contour_pix.append([xline[d],yline[d]])
+
         import copy
         cc=copy.deepcopy(contour_pix)
         filled = []
@@ -88,7 +94,6 @@ def file2cells(filename):
         i[coo[0],coo[1],0]=1
         i[:,:,0] = scipy.ndimage.morphology.binary_fill_holes(i[:,:,0])
         i=numpy.cast['uint8'](255*i)
-        import os
         Image.fromarray(i).save('/tmp/1/c_{0}_{1}_{2}.png'.format(os.path.basename(filename), c['row'],c['col']))
         soma_roi=numpy.nonzero(i[:,:,0])
         if soma_roi[0].shape[0]>maxcellradius**2*numpy.pi*0.1:
@@ -104,15 +109,51 @@ def file2cells(filename):
     return soma_rois
     
 
+    
+
 if __name__ == "__main__":
     folder='/mnt/rzws/dataslow/rajib/'
     folder='/home/rz/codes/data/rajib/'
     ct=1
+    curves=[]
     for f in fileop.listdir_fullpath(folder):
         if 'tif' not in f: continue
 #        if '006' not in f: continue
         with introspect.Timer():
-            print len(file2cells(f))
+            sr=file2cells(f)
+            image=tifffile.imread(f)
+            img=image.reshape((image.shape[0],1, image.shape[1],image.shape[2]))
+#            bg=cone_data.calculate_background(img)
+            bgmask=numpy.ones((image.shape[1],image.shape[2]))
+            for sri in sr:
+                bgmask[sri[0],sri[1]]=0
+            bgmask=scipy.ndimage.morphology.binary_erosion(bgmask,iterations=20,border_value=1)
+            bg_activity=numpy.cast['float']((image*bgmask).mean(axis=1).mean(axis=1))
+            from scipy.ndimage.filters import gaussian_filter
+            bg=gaussian_filter(img.mean(axis=0)[0],150)#sigma is much bigger than cell size
+            roi_curves = experiment_data.get_roi_curves(img-bg,sr)
+#            roi_curves= [rc-bg_activity for rc in roi_curves]
+            for i in range(len(roi_curves)):
+                figure(1)
+                clf()
+                ima=numpy.zeros((image.shape[1],image.shape[2],3))
+                ima[:,:,1]=signal.scale(image.mean(axis=0))
+                ima[sr[i][0],sr[i][1],2]=0.4
+                subplot(2,1,1)
+                imshow(ima)
+                subplot(2,1,2)
+                baseline=roi_curves[i][:frame_rate*5].mean()
+                reponse_size=(roi_curves[i].max()-baseline)/baseline
+                plot(roi_curves[i])
+                title(reponse_size)
+                savefig('/tmp/3/{0}_{1}.png'.format(os.path.basename(f),i))
+                curves.append(roi_curves[i])
+    figure(2)
+    [plot(c) for c in curves];show()
+            
+            
+            
+#            plot(roi_curves);show()
 #        figure(ct);
 #        subplot(1,2,1)
 #        imshow(im);
@@ -124,4 +165,4 @@ if __name__ == "__main__":
 #        title(f);
 #        ct+=1
         
-    show()
+#    show()
