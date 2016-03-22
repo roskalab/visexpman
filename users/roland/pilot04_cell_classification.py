10f33743a6ba6373a6d97378c9d141aa19cd573a
Roland Diggelmann <roland.diggelmann@bsse.ethz.ch>
2016-03-22 16:01:37 +0100

    pilot04 changed

--- users/roland/pilot04_cell_classification.py
+++ users/roland/pilot04_cell_classification.py
@@ -31,7 +31,7 @@ class Pilot04BatchConfig(experiment.ExperimentConfig):
         self.VARS['FingerPrinting']['SPEEDS'] = [300.0]    
         self.VARS['FingerPrinting']['DURATION'] = 15.0
         self.VARS['FingerPrinting']['INTENSITY_LEVELS'] = 255
-        self.VARS['FingerPrinting']['REPEATS'] = 2
+        self.VARS['FingerPrinting']['REPEATS'] = 3
         
         self.STIM_TYPE_CLASS['Chirp_Sweep'] = 'ChirpSweep'
         self.VARS['Chirp_Sweep'] = {}
@@ -69,20 +69,20 @@ class Pilot04BatchConfig(experiment.ExperimentConfig):
         self.VARS['Gratings']['GRATING_STAND_TIME'] = 1.0
         self.VARS['Gratings']['ORIENTATIONS'] = range(0,360,45)
         self.VARS['Gratings']['WHITE_BAR_WIDTHS'] = [100]
-        self.VARS['Gratings']['VELOCITIES'] = [300] #[100, 400, 1600]
+        self.VARS['Gratings']['VELOCITIES'] = [300]
         self.VARS['Gratings']['DUTY_CYCLES'] = [1]
-        self.VARS['Gratings']['PAUSE_BEFORE_AFTER'] = 1.0
+        self.VARS['Gratings']['PAUSE_BEFORE_AFTER'] = 1.0 
         
         self.STIM_TYPE_CLASS['DashStimulus'] = 'DashStimulus'
         self.VARS['DashStimulus'] = {}
         self.VARS['DashStimulus']['BARSIZE'] = [20, 50]
         self.VARS['DashStimulus']['GAPSIZE'] = [5, 30]
         self.VARS['DashStimulus']['MOVINGLINES'] = 3
-        self.VARS['DashStimulus']['DURATION'] = 0.5#10.0
+        self.VARS['DashStimulus']['DURATION'] = 10.0
         self.VARS['DashStimulus']['SPEEDS'] = [300]
         self.VARS['DashStimulus']['DIRECTIONS'] = range(0,360,45)
-        self.VARS['DashStimulus']['BAR_COLOR'] = 0.5 
-        self.VARS['DashStimulus']['REPETITIONS'] = 1
+        self.VARS['DashStimulus']['BAR_COLOR'] = 0.5
+        self.VARS['DashStimulus']['REPETITIONS'] = 3 # ==> 12 min
        
         self.runnable = 'BatchStimulus'
         self._create_parameters_from_locals(locals())
