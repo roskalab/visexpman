61163f6d9321bbd467b5a0d215f39a2e80f5662e
Zoltan Raics <zoltan.raics@raicszoltan.hu>
2016-04-28 22:24:33 +0200

    bugfix

--- users/common/behavioral_setup.py
+++ users/common/behavioral_setup.py
@@ -2,9 +2,9 @@ from visexpman.engine.vision_experiment.configuration import BehavioralConfig
 from visexpman.engine.generic import fileop
 
 class BehavioralSetup(BehavioralConfig):
-        LOG_PATH = fileop.select_folder_exists(['q:\\log', '/tmp'])
-        EXPERIMENT_DATA_PATH = fileop.select_folder_exists(['q:\\data', '/tmp'])
-        CONTEXT_PATH = fileop.select_folder_exists(['q:\\context', '/tmp'])
+        LOG_PATH = fileop.select_folder_exists(['q:\\log', '/tmp', 'c:\\Users\\rz\\tmp'])
+        EXPERIMENT_DATA_PATH = fileop.select_folder_exists(['q:\\data', '/tmp', 'c:\\Users\\rz\\tmp'])
+        CONTEXT_PATH = fileop.select_folder_exists(['q:\\context', '/tmp', 'c:\\Users\\rz\\tmp'])
         ENABLE_CAMERA=True
         CAMERA_FRAME_RATE=7
         CAMERA_FRAME_WIDTH=640/2
