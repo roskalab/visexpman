5468329343dcbb69e14b338492abe54c328d5c02
rdiggelmann <roland.diggelmann@bsse.ethz.ch>
2016-05-11 11:50:03 +0200

    Update mea2016.py

--- users/roland/mea2016.py
+++ users/roland/mea2016.py
@@ -35,7 +35,7 @@ class Mea2016BatchConfig(experiment.ExperimentConfig):
         self.STIM_TYPE_CLASS['WhiteNoise'] = 'WhiteNoiseStimulus'
         self.VARS['WhiteNoise'] = {}
         self.VARS['WhiteNoise']['DURATION_MINS'] = 30.0 # min
-        self.VARS['WhiteNoise']['PIXEL_SIZE'] = [25.0] # um
+        self.VARS['WhiteNoise']['PIXEL_SIZE'] = [40.0] # um (previously: 25)
         self.VARS['WhiteNoise']['N_WHITE_PIXELS'] = False
         self.VARS['WhiteNoise']['COLORS'] = [[0.0, 0.0, 0.0],[0.0, 1.0, 1.0]]
         
