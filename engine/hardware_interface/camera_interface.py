b87dca36616cdc024ef5ce79c3949b71c4f3e27f
hd <hd@comparer1.fmi.ch>
2016-09-26 16:20:08 +0200

    Finished multithreaded/multiprocessing Ximea class

--- engine/hardware_interface/camera_interface.py
+++ engine/hardware_interface/camera_interface.py
@@ -37,6 +37,7 @@ class VideoCamera(instrument.Instrument):
         self.config = config
         self.debug=debug
         self._init_camera()
+        super(VideoCamera, self).__init__(config=config)
         
     def start(self):
         pass
