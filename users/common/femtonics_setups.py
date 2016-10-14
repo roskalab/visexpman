afdf6378899085b286fa0018a99babacebc51675
rzoli <zoltan.raics@raicszoltan.hu>
2016-10-14 16:41:14 +0200

    femotnics ao nhp setup

--- users/common/femtonics_setups.py
+++ users/common/femtonics_setups.py
@@ -59,4 +59,45 @@ class AOSetup(AoCorticalCaImagingConfig):
         self.GAMMA_CORRECTION = copy.deepcopy(hdf5io.read_item(os.path.join(self.CONTEXT_PATH, 'gamma_ao_cortical_monitor.hdf5'), 'gamma_correction'))
         
         
+class CameronBpSetup(AoCorticalCaImagingConfig):
+    def _set_user_parameters(self):
+        AoCorticalCaImagingConfig._set_user_parameters(self)
+        self.PLATFORM='standalone'
+        self.KEYS['start stimulus'] = 'e'
+        # Files
+        self.EXPERIMENT_FILE_FORMAT = 'hdf5'
+        self.LOG_PATH = 'd:\\log'
+        self.EXPERIMENT_DATA_PATH = 'd:\\experiment_data'
+        self.CONTEXT_PATH='d:\\context'
+        #Stimulus screen
+        self.SCREEN_RESOLUTION = utils.cr([1024, 768])
+        self.SCREEN_MAX_FRAME_RATE = 60.0
+        self.FULLSCREEN=not True
+        self.COORDINATE_SYSTEM='center'
+        self.ENABLE_FRAME_CAPTURE = False
+        stim_computer_ip = '127.0.0.1'
+        self.COMMAND_RELAY_SERVER={}
+        self.COMMAND_RELAY_SERVER['RELAY_SERVER_IP'] = stim_computer_ip
+        self.COMMAND_RELAY_SERVER['TIMEOUT'] = 5
+        self.COMMAND_RELAY_SERVER['CLIENTS_ENABLE'] = True
+        self.COMMAND_RELAY_SERVER['ENABLE'] = True
+        self.COMMAND_RELAY_SERVER['RELAY_SERVER_IP_FROM_TABLE'] = True
+        self.COMMAND_RELAY_SERVER['CONNECTION_MATRIX'] = \
+            {
+            'STIM_MES'  : {'STIM' : {'IP': stim_computer_ip, 'LOCAL_IP': '', 'PORT': 10002}, 'MES' : {'IP': '', 'LOCAL_IP': '', 'PORT': 10003}}, 
+            }
+        self.COMMAND_RELAY_SERVER['SERVER_IP'] = {\
+                     'GUI_MES': ['',''],
+                     'STIM_MES': ['',''],
+                     'GUI_STIM': ['', stim_computer_ip],
+                     'GUI_ANALYSIS'  : ['', stim_computer_ip],
+        }   
+        self.DIGITAL_IO_PORT='COM1'
+        self.BLOCK_TRIGGER_PIN = 1
+        self.FRAME_TRIGGER_PIN = 0
         
+#        self.MES_RECORD_OVERHEAD=12
+#        self.MES_RECORD_START_WAITTIME=6
+#        self.SYNC_RECORD_OVERHEAD=5
+        SCREEN_UM_TO_PIXEL_SCALE = 1
+        #self.GAMMA_CORRECTION = copy.deepcopy(hdf5io.read_item(os.path.join(self.CONTEXT_PATH, 'gamma_ao_cortical_monitor.hdf5'), 'gamma_correction'))
