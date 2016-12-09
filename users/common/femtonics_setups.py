93490af21bf4f0a7696644d14f91ee154a6b0aeb
Zoltan Raics <zoltan.raics@raicszoltan.hu>
2016-12-09 14:36:18 +0100

    ao bugfixes

--- users/common/femtonics_setups.py
+++ users/common/femtonics_setups.py
@@ -18,8 +18,9 @@ class AOSetup(AoCorticalCaImagingConfig):
         self.SCREEN_PIXEL_WIDTH = 477.0/self.SCREEN_RESOLUTION ['col']
         self.SCREEN_EXPECTED_FRAME_RATE = 60.0
         self.SCREEN_MAX_FRAME_RATE = 60.0
+        self.IMAGE_DIRECTLY_PROJECTED_ON_RETINA=False
         self.FULLSCREEN=True
-        self.COORDINATE_SYSTEM='ulcorner'
+        self.COORDINATE_SYSTEM='center'
         self.ENABLE_FRAME_CAPTURE = False
         self.GUI['SIZE'] =  utils.cr((1024,768)) 
         #Network
