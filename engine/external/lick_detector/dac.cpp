826280aed3fefd1e16d1cc8c7d072f11f868588e
rzoli <zoltan.raics@raicszoltan.hu>
2017-01-17 14:59:55 +0100

    dac/spi fixes: fixed time wait for spi transaction completition instead waiting for the spi ready flag

--- engine/external/lick_detector/dac.cpp
+++ engine/external/lick_detector/dac.cpp
@@ -4,20 +4,24 @@
 
 Dac::Dac(void)
 {
-  DDRB&=(1<<5)|(1<<3);//SCK and MOSI outputs
-  SPCR = (1<<SPE)|(1<<MSTR)|(1<<SPR0);
+  pinMode(13, OUTPUT);//SCK and MOSI outputs
+  pinMode(11, OUTPUT);
+  SPCR = (1<<SPE)|(1<<MSTR)|(1<<SPR1)|(1<<SPR0);//fclk/128 =115200 Hz ->70 us byte time
+  pinMode(SLAVESELECTPIN,OUTPUT);
   digitalWrite(SLAVESELECTPIN, HIGH);//goes to init
+  pinMode(7, OUTPUT);
+  digitalWrite(7, LOW);//LOW: Select dac route with analog switch
     
 }
 
 void Dac::set(float voltage)
 {    
-    char w1,w2;
+    byte w1,w2;
     int dac_value;
     dac_value=(int)(1000*voltage);//1mV =  1 dac value
     w1=1<<4;//shutdown inv=1, channel A, gain = 2, 1 dac = 1 mV
-    w1|=(char)((dac_value&0x0F00)>>8);
-    w2=(char)(dac_value&0x00FF);
+    w1|=(byte)((dac_value&0x0F00)>>8);
+    w2=(byte)(dac_value&0x00FF);
     digitalWrite(SLAVESELECTPIN, LOW);
     spi_transfer(w1);
     spi_transfer(w2);
@@ -27,7 +31,8 @@ void Dac::set(float voltage)
 void Dac::spi_transfer(char c)
 {
   SPDR = c;
-  while (!(SPSR & (1<<SPIF)))
-    ;
+  delayMicroseconds(70);
+  //while (!(SPSR & (1<<SPIF)))
+  //  ;
 }
 
