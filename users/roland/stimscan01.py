c06cd187f55e4d1245c9b2a7bf63cc50f7a59fd8
Roland Diggelmann <roland.diggelmann@bsse.ethz.ch>
2017-03-17 10:20:01 +0100

    bugfix in stimscan01

--- users/roland/stimscan01.py
+++ users/roland/stimscan01.py
@@ -56,7 +56,7 @@ class StimScan01WhiteNoise(experiment.ExperimentConfig):
     def _create_parameters(self):
         self.runnable = 'WhiteNoiseStimulus'
         self.DURATION_MINS = 30.0 #30 min
-        self.PIXEL_SIZE = 40.0
+        self.PIXEL_SIZE = [40.0]
         #self.FLICKERING_FREQUENCY = 60.0
         self.N_WHITE_PIXELS = False
         self.COLORS = [[0.0, 0.0, 0.0],[0.0, 1.0, 1.0]]
