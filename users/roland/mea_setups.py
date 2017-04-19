8116ea4370c27ded8d09231cbb5d2fe776957735
Roland Diggelmann <roland.diggelmann@bsse.ethz.ch>
2017-04-19 17:30:19 +0200

    mea_setup back to experiment mode

--- users/roland/mea_setups.py
+++ users/roland/mea_setups.py
@@ -33,7 +33,7 @@ class MEAConfig(HiMEAConfig):
         #recordingMachineName = 'bs-hpws18' # hungary setup
         recordingMachineName = 'bs-hpws19' # retina room
         folder = '/mnt/' + recordingMachineName + '/' + username + '/' + time.strftime('%y%m%d'.format(os.sep)) + '/'
-        folder = '/home/rolandd/tmp/'        
+        #folder = '/home/rolandd/tmp/'        
         
         
         if not os.path.isdir(folder):
