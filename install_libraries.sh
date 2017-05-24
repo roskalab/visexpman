4408f1096d20f4db058bdeb575d54b92b670d4a6
rolandd <roland.diggelmann@bsse.ethz.ch>
2017-05-24 17:07:19 +0200

    install script updated

--- install_libraries.sh
+++ install_libraries.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-sudo apt-get install python-psutil python-numpy python-scipy python-qwt5-qt4 python-zc.lockfile python-tables python-pygame
+sudo apt-get install python-parallel python-serial python-pip python-psutil python-numpy python-scipy python-qwt5-qt4 python-zc.lockfile python-tables python-pygame python-pyqtgraph
+
+pip install pyzmq
 
 
 # In order to use the parallel port, change the driver:
 sudo rmmod lp
+sudo rmmod parport_pc
+
 sudo modprobe ppdev
