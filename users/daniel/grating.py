4740a56c944e5d925544eb196ef318510ae68ae9
hd <hd@rlvivo1>
2017-06-09 17:22:27 +0200

    Stimulus modifications automatically saved

--- users/daniel/grating.py
+++ users/daniel/grating.py
@@ -7,6 +7,26 @@ import random
 import copy
 
 
+class MovingGratingQuickMarchConfig(experiment.ExperimentConfig):
+    def _create_parameters(self):
+        #Timing
+        self.NUMBER_OF_MARCHING_PHASES = 4 #number of static bar compositions at beginning
+        self.NUMBER_OF_BAR_ADVANCE_OVER_POINT = 3 #how many times the bar hit a point -> this + speed = moving time
+        self.MARCH_TIME = 2 # standing phase time
+        self.GRATING_STAND_TIME = 2.0 #post-moving-phase time
+        #Grating parameters
+        self.ORIENTATIONS = range(0, 360, 45)
+        self.STARTING_PHASES = [0]*len(self.ORIENTATIONS)
+        self.WHITE_BAR_WIDTHS = [300.0]
+        self.VELOCITIES = [1200.0]
+        self.DUTY_CYCLES = [3.] #white and blck bar ratio -> number of bars
+        self.REPEATS = 2
+        self.PAUSE_BEFORE_AFTER = 1.0 #very beginning and end witing time
+        self.COLOR_CONTRAST = 1.0
+        self.runnable = 'MovingGrating'
+        self.pre_runnable = 'MovingGratingPre'
+        self._create_parameters_from_locals(locals())
+
 class PhasesGratingConfig(grating_base.MovingGratingConfig):
     def _create_parameters(self):
         grating_base.MovingGratingConfig._create_parameters(self)
