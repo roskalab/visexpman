7072ef4fcd88482beb4447ceb845284eb952ba11
Zoltan Raics <zoltan.raics@raicszoltan.hu>
2017-06-12 16:43:29 +0200

    receptive field test stimulus added

--- users/zoltan/experiment.py
+++ users/zoltan/experiment.py
@@ -1,5 +1,6 @@
 import numpy
 from visexpman.users.common import grating_base
+from visexpman.engine.vision_experiment import experiment
 
 class MyMovingGratingConfig(grating_base.MovingGratingNoMarchingConfig):
     pass
@@ -50,3 +51,66 @@ class PhasesGratingConfig(grating_base.MovingGratingConfig):
         self.runnable = 'MovingGrating'
         self.pre_runnable = 'MovingGratingPre'
         self._create_parameters_from_locals(locals())
+
+from visexpman.engine.generic import utils
+class ReceptiveFieldTest(experiment.ExperimentConfig):
+    def _create_parameters(self):
+	# x = 4248 um,  y = 2389 um
+        self.SHAPE = 'rect'
+        self.COLORS = [1.0]
+        self.BACKGROUND_COLOR = 0
+        self.NROWS = 3#10
+        self.NCOLUMNS = 6#18
+        self.SIZE_DIMENSION='angle'
+        self.DISPLAY_SIZE = utils.rc((50.0,90.0))
+        self.DISPLAY_CENTER = utils.rc((25,45.0))
+        self.ON_TIME = 2.0#0.8
+        self.OFF_TIME = 0#0.8
+        self.REPEATS = 1 
+        self.REPEAT_SEQUENCE = 1
+        self.ENABLE_RANDOM_ORDER =  False
+        self.runnable='ReceptiveFieldExploreZ'
+        self._create_parameters_from_locals(locals())
+
+
+class ReceptiveFieldExploreZ(experiment.Experiment):
+    '''
+    Repeats: flash or sequence
+    
+    Supported use cases: fixed size squares are presented with no gaps and no overlaps. Fractional squares are not shown at the edges
+    '''
+    def prepare(self):
+#	print self.machine_config.SCREEN_SIZE_UM
+        shape_size, nrows, ncolumns, display_size, shape_colors, background_color = \
+                self._parse_receptive_field_parameters(self.experiment_config.SHAPE_SIZE if hasattr(self.experiment_config, 'SHAPE_SIZE') else None,
+                                                    self.experiment_config.NROWS if hasattr(self.experiment_config, 'NROWS') else None,
+                                                    self.experiment_config.NCOLUMNS if hasattr(self.experiment_config, 'NCOLUMNS') else None,
+                                                    self.experiment_config.DISPLAY_SIZE if hasattr(self.experiment_config, 'DISPLAY_SIZE') else None,
+                                                    self.experiment_config.COLORS,
+                                                    self.experiment_config.BACKGROUND_COLOR)
+        self.stimulus_duration, positions= self.receptive_field_explore_durations_and_positions(shape_size=shape_size, 
+                                                                            nrows = nrows,
+                                                                            ncolumns = ncolumns,
+                                                                            shape_colors = shape_colors,
+                                                                            flash_repeat = self.experiment_config.REPEATS,
+                                                                            sequence_repeat = self.experiment_config.REPEAT_SEQUENCE,
+                                                                            on_time = self.experiment_config.ON_TIME,
+                                                                            off_time = self.experiment_config.OFF_TIME)
+        self.fragment_durations=[self.stimulus_duration]
+        
+            
+    def run(self):
+        self.receptive_field_explore(self.experiment_config.SHAPE_SIZE if hasattr(self.experiment_config, 'SHAPE_SIZE') else None, 
+                                    self.experiment_config.ON_TIME,
+                                    self.experiment_config.OFF_TIME,
+                                    nrows = self.experiment_config.NROWS if hasattr(self.experiment_config, 'NROWS') else None,
+                                    ncolumns = self.experiment_config.NCOLUMNS if hasattr(self.experiment_config, 'NCOLUMNS') else None,
+                                    display_size = self.experiment_config.DISPLAY_SIZE if hasattr(self.experiment_config, 'DISPLAY_SIZE') else None,
+                                    flash_repeat = self.experiment_config.REPEATS, 
+                                    sequence_repeat = self.experiment_config.REPEAT_SEQUENCE,
+                                    background_color = self.experiment_config.BACKGROUND_COLOR, 
+                                    shape_colors = self.experiment_config.COLORS, 
+                                    random_order = self.experiment_config.ENABLE_RANDOM_ORDER)
+        self.show_fullscreen(color = self.experiment_config.BACKGROUND_COLOR)
+        #print self.shape_size,self.machine_config.SCREEN_SIZE_UM,self.ncolumns,self.nrows
+        self.user_data = { 'nrows':self.nrows,  'ncolumns': self.ncolumns,  'shape_size':self.shape_size}
