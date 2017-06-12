d996af84dfe0dfff8d7f0d6779658ba222308f52
Zoltan Raics <zoltan.raics@raicszoltan.hu>
2017-06-12 17:22:07 +0200

    receptive field tester stimulus added

--- users/zoltan/experiment_tests.py
+++ users/zoltan/experiment_tests.py
@@ -4,6 +4,67 @@ import numpy,time
 from visexpman.engine.generic import utils,geometry,colors
 from visexpman.engine.vision_experiment import experiment
 
+class ReceptiveFieldTest(experiment.ExperimentConfig):
+    def _create_parameters(self):
+        self.SHAPE = 'rect'
+        self.COLORS = [1.0]
+        self.BACKGROUND_COLOR = 0
+        self.NROWS = 3#10
+        self.NCOLUMNS = 6#18
+        self.SIZE_DIMENSION='angle'
+        self.DISPLAY_SIZE = utils.rc((50.0,90.0))
+        self.DISPLAY_CENTER = utils.rc((25,45.0))
+        self.ON_TIME = 2.0#0.8
+        self.OFF_TIME = 0#0.8
+        self.REPEATS = 1 
+        self.REPEAT_SEQUENCE = 1
+        self.ENABLE_RANDOM_ORDER =  False
+        self.runnable='ReceptiveFieldExploreZ'
+        self._create_parameters_from_locals(locals())
+
+
+class ReceptiveFieldExploreZ(experiment.Experiment):
+    '''
+    Repeats: flash or sequence
+    
+    Supported use cases: fixed size squares are presented with no gaps and no overlaps. Fractional squares are not shown at the edges
+    '''
+    def prepare(self):
+#	print self.machine_config.SCREEN_SIZE_UM
+        shape_size, nrows, ncolumns, display_size, shape_colors, background_color = \
+                self._parse_receptive_field_parameters(self.experiment_config.SHAPE_SIZE if hasattr(self.experiment_config, 'SHAPE_SIZE') else None,
+                                                    self.experiment_config.NROWS if hasattr(self.experiment_config, 'NROWS') else None,
+                                                    self.experiment_config.NCOLUMNS if hasattr(self.experiment_config, 'NCOLUMNS') else None,
+                                                    self.experiment_config.DISPLAY_SIZE if hasattr(self.experiment_config, 'DISPLAY_SIZE') else None,
+                                                    self.experiment_config.COLORS,
+                                                    self.experiment_config.BACKGROUND_COLOR)
+        self.stimulus_duration, positions= self.receptive_field_explore_durations_and_positions(shape_size=shape_size, 
+                                                                            nrows = nrows,
+                                                                            ncolumns = ncolumns,
+                                                                            shape_colors = shape_colors,
+                                                                            flash_repeat = self.experiment_config.REPEATS,
+                                                                            sequence_repeat = self.experiment_config.REPEAT_SEQUENCE,
+                                                                            on_time = self.experiment_config.ON_TIME,
+                                                                            off_time = self.experiment_config.OFF_TIME)
+        self.fragment_durations=[self.stimulus_duration]
+        
+            
+    def run(self):
+        self.receptive_field_explore(self.experiment_config.SHAPE_SIZE if hasattr(self.experiment_config, 'SHAPE_SIZE') else None, 
+                                    self.experiment_config.ON_TIME,
+                                    self.experiment_config.OFF_TIME,
+                                    nrows = self.experiment_config.NROWS if hasattr(self.experiment_config, 'NROWS') else None,
+                                    ncolumns = self.experiment_config.NCOLUMNS if hasattr(self.experiment_config, 'NCOLUMNS') else None,
+                                    display_size = self.experiment_config.DISPLAY_SIZE if hasattr(self.experiment_config, 'DISPLAY_SIZE') else None,
+                                    flash_repeat = self.experiment_config.REPEATS, 
+                                    sequence_repeat = self.experiment_config.REPEAT_SEQUENCE,
+                                    background_color = self.experiment_config.BACKGROUND_COLOR, 
+                                    shape_colors = self.experiment_config.COLORS, 
+                                    random_order = self.experiment_config.ENABLE_RANDOM_ORDER)
+        self.show_fullscreen(color = self.experiment_config.BACKGROUND_COLOR)
+        #print self.shape_size,self.machine_config.SCREEN_SIZE_UM,self.ncolumns,self.nrows
+        self.user_data = { 'nrows':self.nrows,  'ncolumns': self.ncolumns,  'shape_size':self.shape_size}
+
 class NaturalBarsExperiment1(experiment.Stimulus):
     def stimulus_configuration(self):
         self.SPEED = [800, 400,1500.0]#um/s
@@ -135,4 +196,4 @@ class Gr(experiment.Stimulus):
 
 if __name__ == "__main__":
     from visexpman.engine.visexp_app import stimulation_tester
-    stimulation_tester('zoltan', 'StimulusDevelopment', 'Gr')
+    stimulation_tester('zoltan', 'StimulusDevelopment', 'ReceptiveFieldTest')
