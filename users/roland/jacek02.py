76b2b72244ae684c8e10e915149d00de28b0d04c
rolandd <roland.diggelmann@bsse.ethz.ch>
2017-06-23 14:32:26 +0200

    new bar stimulus

---
+++ users/roland/jacek02.py
@@ -0,0 +1,50 @@
+"""
+Created on Mon May 29 18:30:00 2017
+@author: rolandd
+"""
+from visexpman.engine.generic import utils
+from visexpman.engine.vision_experiment import experiment
+
+# ------------------------------------------------------------------------------
+class J02MovingBar(experiment.ExperimentConfig):
+    def _create_parameters(self):
+        self.runnable = 'MovingShapeStimulus'
+        self.DIRECTIONS = range(0, 360, 45)
+        self.SPEEDS = [300, 1600]  # um/s#
+        self.REPETITIONS = 6
+        self.SHAPE_BACKGROUND = 0.0
+        self.SHAPE_CONTRAST = 1.0
+        
+        self.SHAPE = 'rect'
+        self.SHAPE_SIZE = utils.cr((1000, 500)) #um
+        self.RANDOM_ORDER = True
+        self.PAUSE_BETWEEN_DIRECTIONS = 1.0
+        self._create_parameters_from_locals(locals())
+
+# ------------------------------------------------------------------------------
+class J02MarchingSquaresSmall(experiment.ExperimentConfig):
+    def _create_parameters(self):
+        self.runnable='ReceptiveFieldExplore'
+        self.SHAPE = 'rect'
+        self.COLORS = [1.0, 0.0] # black, white
+        self.BACKGROUND_COLOR = 0.5 # grey
+        self.SHAPE_SIZE = 200.0 # um
+        self.ON_TIME = 1.0 
+        self.OFF_TIME = 1.0
+        self.PAUSE_BEFORE_AFTER = 2.0
+        self.REPEATS = 3
+        self.REPEAT_SEQUENCE = 2
+        self.ENABLE_RANDOM_ORDER = True
+        self._create_parameters_from_locals(locals())
+
+# ------------------------------------------------------------------------------
+class J02WhiteNoise(experiment.ExperimentConfig):
+    def _create_parameters(self):
+        self.runnable = 'WhiteNoiseStimulus'
+        self.DURATION_MINS = 15.0 #15 min
+        self.PIXEL_SIZE = [40.0]
+        self.N_WHITE_PIXELS = False
+        self.COLORS = [[0.0, 0.0, 0.0],[0.0, 1.0, 1.0]]
+        self._create_parameters_from_locals(locals())
+        
+        
