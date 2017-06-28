3c4fe74b76985a1ab44565a23d0d638cf6aa0ee1
rz <zoltan.raics@raicszoltan.hu>
2017-06-28 09:24:59 +0200

    ao jobhandler job status handling works, unittest passes

--- engine/jobhandler.py
+++ engine/jobhandler.py
@@ -1,5 +1,5 @@
-import os,hdf5io, tables,unittest,sys,shutil,time,traceback,logging
-from visexpman.engine.generic import fileop,utils
+import os,tables,unittest,sys,shutil,time,traceback,logging,sys
+from visexpman.engine.generic import fileop,utils,introspect
 from visexpman.engine.vision_experiment import experiment_data
 from visexpman.engine.analysis import aod
 
@@ -8,13 +8,19 @@ class AoJobhandler(object):
         self.db=DatafileDatabase(database_filename)
         self.experiment_data_path=experiment_data_path
         self.backup_path=backup_path
-        self.mesfile_minimum_age=10
+        self.mesfile_minimum_age=60
         self.ignore_errors=False
         self.logfile = os.path.join(logpath, 'jobhandler_{0}.txt'.format(utils.timestamp2ymdhm(time.time()).replace(':','').replace(' ','').replace('-','')))
         logging.basicConfig(filename= self.logfile,
                     format='%(asctime)s %(levelname)s\t%(message)s',
 level=logging.INFO)
-        print 'todo: check free space'
+        self.minimum_free_space=15e9/10
+        fs=fileop.free_space(experiment_data_path)
+        self.printl('{0} GB free space is on {1}'.format(int(fs/2**30), experiment_data_path))
+        if fs<self.minimum_free_space:
+            raise RuntimeError('Less than {1} GB space on {0}'.format(experiment_data_path,int(self.minimum_free_space/2**30)))
+        if fileop.free_space(backup_path)<self.minimum_free_space:
+            raise RuntimeError('Less than {1} GB space on {0}'.format(experiment_data_path, int(self.minimum_free_space/2**30)))
         
     
     def printl(self,msg,loglevel='info'):
@@ -37,13 +43,14 @@ level=logging.INFO)
             p=experiment_data.parse_recording_filename(f)
             if self.db.add(filename=p['file'], measurement_ready_time=now):
                 self.db.update(filename=p['file'], measurement_ready=True)
+                self.printl('{0} added'.format(p['file']))
         pass
         
     def fetch_job(self):
-        query='(measurement_ready==1)'
+        query='(measurement_ready==1)&((backed_up==0) | (mesextractor_ready==0) | (converted==0) | (copied==0))'
         if not self.ignore_errors:
             query+='& (error==0)'
-        active_jobs=[[r['filename'], r['backed_up'], r['mesextractor_ready'], r['converted']] for r in self.db.hdf5.root.datafiles.where( query)]
+        active_jobs=[[r['filename'], r['backed_up'], r['mesextractor_ready'], r['converted'], r['copied']] for r in self.db.hdf5.root.datafiles.where( query)]
         if len(active_jobs)==0:
             return
         #Not backed up files has the highest priority
@@ -55,16 +62,18 @@ level=logging.INFO)
             #Find the latest unprocessed
             filenames=[j[0] for j in active_jobs]
             filenames.sort()
-            fn, bu, mesextr, converted=[j for j in active_jobs if j[0]==filenames[-1]][0]
+            fn, bu, mesextr, converted, copied=[j for j in active_jobs if j[0]==filenames[-1]][0]
             next_job=[fn]
             if not mesextr:
                 next_job.append('mesextractor')
             elif not converted:
                 next_job.append('convert')
+            elif not copied:
+                next_job.append('copy')
         #Get full filename
         hdf5files=fileop.find_files_and_folders(self.experiment_data_path,extension='hdf5')[1]
         fullpath=[f for f in hdf5files if os.path.basename(f)==next_job[0]][0]
-        try:
+        try:            
             getattr(self, next_job[1])(fullpath)
             now=time.time()
             kwargs={'filename':next_job[0]}
@@ -77,14 +86,18 @@ level=logging.INFO)
             elif next_job[1]=='convert':
                 kwargs['converted']=True
                 kwargs['converted_time']=now
+            elif next_job[1]=='copy':
+                kwargs['copied']=True
+                kwargs['copied_time']=now
             self.db.update(**kwargs)
+            self.printl('{0}/{1} job done'.format(*next_job))
         except:
             self.db.update(filename=next_job[0], error=True, error_message=next_job[1])
             self.printl(traceback.format_exc(),'error')
 
     def backup(self, filename):
         mesfilename=filename.replace('.hdf5', '.mat')
-        dst=os.path.join(self.backup_path, *mesfilename.split(os.sep)[-3:-1])
+        dst=os.path.join(self.backup_path, 'raw', *mesfilename.split(os.sep)[-3:-1])
         if not os.path.exists(dst):
             os.makedirs(dst)
         shutil.copy(mesfilename, dst)
@@ -94,7 +107,28 @@ level=logging.INFO)
             raise RuntimeError('{0} not backed up'.format(filename))
         
     def mesextractor(self,filename):
+        #Check if input files are valid
+        files=[filename, filename.replace('.hdf5', '.mat')]
+        for f in files:
+            if os.path.getsize(f)<1e6:
+                raise IOError('{0} is corrupt'.format(f))
+        if introspect.is_test_running(): return
         aod.AOData(filename)
+        
+    def convert(self,filename):
+        experiment_data.hdf52mat(filename)
+    
+    def copy(self,filename):
+        dst=os.path.join(self.backup_path, 'processed', *filename.split(os.sep)[-3:-1])
+        if not os.path.exists(dst):
+            os.makedirs(dst)
+        files2copy=[filename, filename.replace('.hdf5', '_mat.mat')]
+        for f in files2copy:
+            shutil.copy(f, dst) 
+            
+    def close(self):
+        self.printl('Jobhandler closed')
+        self.db.close()
         
 
 class Datafile(tables.IsDescription):
@@ -108,6 +142,8 @@ class Datafile(tables.IsDescription):
     mesextractor_time = tables.Float64Col()
     converted = tables.BoolCol()
     converted_time = tables.Float64Col()
+    copied = tables.BoolCol()
+    copied_time = tables.Float64Col()
     error = tables.BoolCol()
     error_message=tables.StringCol(256)
     filename=tables.StringCol(256)
@@ -173,37 +209,60 @@ class TestJobhandler(unittest.TestCase):
         dbfilename=os.path.join(root, 'jobs.hdf5')
         experiment_data_path=os.path.join(root, 'data', 'user', '20170101')
         os.makedirs(experiment_data_path)
-        nfiles=10
+        backup_path=os.path.join(root, 'bu')
+        budst=os.path.join(backup_path,'raw', 'user', '20170101')
+        os.makedirs(backup_path)
+        os.makedirs(budst)
+        nfiles=30
         stimname='TestStim'
         now=time.time()
+        import hdf5io, numpy, scipy.io
+        validfiles=[]
         for i in range(nfiles):
-            idn=experiment_data.get_id(now-i)
+            idn=experiment_data.get_id(now-i*0.1)
             fn=os.path.join(experiment_data_path, '_'.join(['data', stimname, idn])+'.hdf5')
-            fileop.write_text_file(fn,10*'a')
+            
+            h=hdf5io.Hdf5io(fn)
+            h.data=numpy.random.random((1000,100,10))
+            h.save('data')
+            h.close()
             if i%2==0:
-                fileop.write_text_file(fn.replace('.hdf5', '.mat'),100*'a')
-        backup_path=os.path.join(root, 'bu')
-        os.mkdir(backup_path)
+                data={'data':numpy.random.random((1000,100,10))}
+                validfiles.append(fn)
+                scipy.io.savemat(fn.replace('.hdf5', '.mat'), data)
+                shutil.copy(fn, budst)#Mimic that hdf5 files already backed up
+            
+        
         logpath='/tmp'
         jh=AoJobhandler(experiment_data_path,backup_path, logpath,dbfilename)
         jh.add_jobs()
-        self.assertEqual(len(jh.db.hdf5.root.datafiles), 0)
+        self.assertTrue(len(jh.db.hdf5.root.datafiles)< nfiles/2)
         time.sleep(jh.mesfile_minimum_age+0.1)
         jh.add_jobs()
         self.assertEqual(len(jh.db.hdf5.root.datafiles), nfiles/2)
         jh.add_jobs()
-        for i in range(10):
+        for i in range(nfiles/2*4+2):
+            jh.add_jobs()
             jh.fetch_job()
-        pass
-        pass
-            
-            
-        
+        jh.close()
+        log=fileop.read_text_file(jh.logfile)
+        self.assertFalse('error' in log)
+        lines=log.split('\n')
+        for vf in validfiles:
+            lines2vf=[l for l in lines if os.path.basename(vf) in l]
+            jobstates=['copy', 'convert', 'mesextractor', 'backup']
+            ct=0
+            for kw in jobstates:
+                ct+=len([1 for l in lines2vf if kw+' job done' in l])
+            self.assertEqual(ct, len(jobstates))
     
-
-
 if __name__=='__main__':
     if len(sys.argv)==1:
         unittest.main()
     else:
-        pass
+        jh=AoJobhandler(*sys.argv[1:])
+        while True:
+            if utils.enter_hit(): break
+            jh.add_jobs()
+            jh.fetch_job()
+            time.sleep(0.1)
