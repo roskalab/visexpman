c9016a6869e0ef3e675ff07fd2fe0b881e93ee86
rz <zoltan.raics@raicszoltan.hu>
2017-07-17 05:00:15 +0200

    natural bars stimulus moved to common area

--- users/common/stimuli.py
+++ users/common/stimuli.py
@@ -185,19 +185,67 @@ class NaturalMovieExperiment(experiment.Experiment):
         else:
             duration = 1.0/self.experiment_config.FRAME_RATE
         self.show_image(self.experiment_config.FILENAME,duration)
+        
+class NaturalBars(experiment.ExperimentConfig):
+    def _create_parameters(self):
+
+        self.SPEED = [800,400,1200]#um/s
+        self.REPEATS = 2 #5
+        self.DIRECTIONS = [0,180] #range(0,360,90)
+        self.DURATION = 12
+        
+        self.BACKGROUND_TIME = 0.5
+        self.BACKGROUND_COLOR = 0.5
+        self.ENABLE_FLYINOUT= True #True # False
+        self.ALWAYS_FLY_IN_OUT = True #True # False
+        
+        #Advanced/Tuning
+        self.MINIMAL_SPATIAL_PERIOD= 120 #None
+        self.SCALE= 1.0
+        self.OFFSET=0.0
+        self.runnable = 'NaturalBarsExperiment'
+        self._create_parameters_from_locals(locals())
 
 class NaturalBarsExperiment(experiment.Experiment):
     def prepare(self):
-        self.fragment_durations = [self.experiment_config.DURATION*self.experiment_config.REPEATS*len(self.experiment_config.DIRECTIONS)]
+        self.fragment_durations = [self.experiment_config.DURATION*self.experiment_config.REPEATS*len(self.experiment_config.DIRECTIONS)*len(self.experiment_config.SPEED)]
+        self.duration=self.fragment_durations[0]
+        print self.duration
         
     def run(self):
         for rep in range(self.experiment_config.REPEATS):
             if self.abort:
                 break
+            print rep
+            self.show_fullscreen(duration = self.experiment_config.BACKGROUND_TIME, color =  self.experiment_config.BACKGROUND_COLOR, flip=True)
             for directions in self.experiment_config.DIRECTIONS:
                 if self.abort:
                     break
-                self.show_natural_bars(speed = self.experiment_config.SPEED, duration=self.experiment_config.DURATION, minimal_spatial_period = None, spatial_resolution = self.machine_config.SCREEN_PIXEL_TO_UM_SCALE, intensity_levels = 255, direction = directions)
+                for speeds in self.experiment_config.SPEED:
+                    if self.abort:
+                        break
+                    if self.experiment_config.ALWAYS_FLY_IN_OUT:
+                        fly_in = True
+                        fly_out = True
+                    else:
+                        if self.experiment_config.SPEED.index(speeds) == 0:
+                            fly_in = True
+                            fly_out = False
+                        elif self.experiment_config.SPEED.index(speeds) == len(self.experiment_config.SPEED)-1:
+                            fly_in = False
+                            fly_out = True
+                        else:
+                            fly_in = False
+                            fly_out = False
+                    if not self.experiment_config.ENABLE_FLYINOUT:
+                        fly_in = False
+                        fly_out = False
+                    self.block_start('natural_bars')
+                    self.show_natural_bars(speed = speeds, duration=self.experiment_config.DURATION, minimal_spatial_period = self.experiment_config.MINIMAL_SPATIAL_PERIOD, spatial_resolution = self.machine_config.SCREEN_PIXEL_TO_UM_SCALE, 
+                            scale=self.experiment_config.SCALE,
+                            offset=self.experiment_config.OFFSET,
+                            intensity_levels = 255, direction = directions, fly_in = fly_in, fly_out = fly_out)
+                    self.block_end('natural_end')
 
 class LaserBeamStimulus(experiment.Experiment):
     def run(self):
