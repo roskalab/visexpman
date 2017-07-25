b5670c367b68b4b03abf95606061edc692e85eed
Zoltan Raics <zoltan.raics@raicszoltan.hu>
2017-07-25 16:59:04 +0200

    ioboard: digital input sampling (not tested)

--- engine/external/ioboard/ioboard.ino
+++ engine/external/ioboard/ioboard.ino
@@ -1,16 +1,58 @@
+#define COMPARE 116//2 kHz, FCPU is 14.7456MHz, comp=FCPU/(f*prescale)+1
+#define PRESCALE 4 //64 prescale
+#define IDLE_ST 0
+#define WAIT_PAR_ST 1
+#define OUTPORT_MASK 0xC0
 
 char b;
 byte state;
-char cmd;
 byte par;
-#define IDLE_ST 0
-#define WAIT_PAR_ST 1
+
+byte cmd = 0;
+byte send_data=true;
+byte port,port_prev;
+unsigned long time;
+bool force_read_pin=false;
+
+ISR(TIMER1_COMPA_vect) {
+   PORTB |=(1<<2);//D10 output
+   TCNT1L=0;
+   TCNT1H=0;
+   time = millis();
+   port=PIND&0xfc;
+   if ((port!=port_prev) || force_read_pin)
+   {
+     if (send_data)
+     {
+       Serial.print(time);
+       Serial.print(" ms: ");
+       Serial.print(port,HEX);
+       Serial.print("\r\n");
+     }
+     force_read_pin=false;
+     port_prev=port;
+   }
+   PORTB &=~(1<<2);
+}
+
+void init_digital_input_timer()
+{
+   TCCR1B = PRESCALE;
+   OCR1AH = 0;
+   OCR1AL = COMPARE;
+   TIMSK1 |= 1<<1;
+}
 
 void setup() {
   Serial.begin(115200);
-  PIND=0xFC;
+  DDRB|=1<<2;//D10 output for debugging
+  DDRD=OUTPORT_MASK;//port 2-4 input, port 5-7 output
   PORTD=0x00;
   state=IDLE_ST;
+  init_digital_input_timer();
+  port_prev=0;
+  port=0;
+  sei();
 }
 
 
@@ -23,10 +65,22 @@ void loop() {
       case IDLE_ST:
         switch (b)
         {
-          case 'd':
-          case 'p':
+          case 'o'://output
+          case 'p'://pulse
             cmd=b;
             state=WAIT_PAR_ST;
+            break;
+          case 'r'://read pins
+            force_read_pin=true;
+            state=IDLE_ST;
+            break;
+          case 'e'://enable send data
+            send_data=true;
+            state=IDLE_ST;
+            break;
+          case 'd'://disable send data
+            send_data=false;
+            state=IDLE_ST;
             break;
           default:
             state=IDLE_ST;
@@ -37,12 +91,12 @@ void loop() {
         par = b;
         switch (cmd) {
           case 'd':
-              PORTD = par&0xFC;
+              PORTD = par&OUTPORT_MASK;
               break;
           case 'p':
-              PORTD |= par&0xFC;
+              PORTD |= par&OUTPORT_MASK;
               delay(2);
-              PORTD &= ~(par&0xFC);
+              PORTD &= ~(par&OUTPORT_MASK);
               break;
           case 'a':
               break;
