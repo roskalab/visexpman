27806432bcc0904050afdd931cc512dfa7186b79
Zoltan Raics <zoltan.raics@raicszoltan.hu>
2017-08-21 21:41:00 +0200

    ir laser stim bugfixes

--- users/common/stimuli.py
+++ users/common/stimuli.py
@@ -24,25 +24,21 @@ class LaserPulse(experiment.ExperimentConfig):
 
 class LaserPulseE(experiment.Experiment):
     def calculate_waveform(self):
-        init=numpy.zeros(int(self.SAMPLE_RATE*self.INITIAL_DELAY))
+        ec=self.experiment_config
+        init=numpy.zeros(int(ec.SAMPLE_RATE*ec.INITIAL_DELAY))
         pulses=[]
-        if len(self.PULSE_DURATION)!=len(self.PERIOD_TIME):
+        if len(ec.PULSE_DURATION)!=len(ec.PERIOD_TIME):
             raise RuntimeError('Invalid timing configuration')
-        for v in self.LASER_AMPLITUDE:
-            for i in range(len(self.PULSE_DURATION)):
-                pulse_duration=self.PULSE_DURATION[i]
-                period_time=self.PERIOD_TIME[i]
-                pulse=numpy.concatenate((numpy.ones(int(self.SAMPLE_RATE*pulse_duration)), numpy.zeros(int(self.SAMPLE_RATE*(period_time-pulse_duration)))))*v
-                pulses.append(numpy.tile(pulse,self.NPULSES))
+        for v in ec.LASER_AMPLITUDE:
+            for i in range(len(ec.PULSE_DURATION)):
+                pulse_duration=ec.PULSE_DURATION[i]
+                period_time=ec.PERIOD_TIME[i]
+                pulse=numpy.concatenate((numpy.ones(int(ec.SAMPLE_RATE*pulse_duration)), numpy.zeros(int(ec.SAMPLE_RATE*(period_time-pulse_duration)))))*v
+                pulses.append(numpy.tile(pulse,ec.NPULSES))
         self.waveform=numpy.concatenate(pulses)
         self.waveform=numpy.concatenate((init, self.waveform))
         timing_waveform=numpy.where(self.waveform==0,0,5)#.reshape(1,self.waveform.shape[0])
-        self.waveform=numpy.where(self.waveform==0.0,self.ZERO_VOLTAGE,self.waveform)
-        if 0:
-                from pylab import plot,savefig,cla,clf
-                clf()
-                cla()
-                plot(self.waveform);savefig('c:\\temp\\fig.png')
+        self.waveform=numpy.where(self.waveform==0.0,ec.ZERO_VOLTAGE,self.waveform)
 #        self.waveform=self.waveform.reshape(1,self.waveform.shape[0])
         self.combined_waveform=numpy.zeros((2,self.waveform.shape[0]))
         self.combined_waveform[0]=self.waveform
@@ -50,12 +46,12 @@ class LaserPulseE(experiment.Experiment):
 
     def prepare(self):
         self.calculate_waveform()
-        self.duration = self.combined_waveform.shape[1]/float(self.SAMPLE_RATE)
+        self.duration = self.combined_waveform.shape[1]/float(self.experiment_config.SAMPLE_RATE)
         self.fragment_durations=[self.duration]
         
     def run(self):
         from visexpman.engine.hardware_interface import daq_instrument
         self.show_fullscreen(color=0.0,duration=0)
 #        self.block_start('laser')
-        daq_instrument.set_waveform('Dev1/ao0:1',self.combined_waveform,sample_rate = self.SAMPLE_RATE)
+        daq_instrument.set_waveform('Dev1/ao0:1',self.combined_waveform,sample_rate = self.experiment_config.SAMPLE_RATE)
 #        self.block_end('laser')
