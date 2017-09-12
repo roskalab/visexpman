c0f83e2d0620635121364746e898a2b52cd87f29
zmatej <matej.znidaric@bsse.ethz.ch>
2017-09-12 10:18:41 +0200

    moving bar stimulus in matej-testing_stimulus changed;

--- users/zmatej/matej-testing_stimuli.py
+++ users/zmatej/matej-testing_stimuli.py
@@ -100,12 +100,15 @@ class FullFieldFlashes(experiment.ExperimentConfig):
 # -----------------------------------------------------------------------------
 class MovingBar(experiment.ExperimentConfig):
     def _create_parameters(self):
-        self.runnable = 'MovingStimulus'
+        self.runnable = 'MovingShapeStimulus'
         self.SHAPE_SIZE = utils.cr((1000, 500)) #um
         self.SPEEDS = [1600] #um/s#
         #self.SPEEDS = [120, 200,400,1600] #um/s
         self.PAUSE_BETWEEN_DIRECTIONS = 1.0
+        self.RANDOM_DIRECTIONS = True
         self.REPETITIONS = 10
         self.DIRECTIONS = range(0,360,45)
+        self.SHAPE = 'rect'
         self.SHAPE_BACKGROUND=0.0
+        self.SHAPE_CONTRAST = 1.0
         self._create_parameters_from_locals(locals())
