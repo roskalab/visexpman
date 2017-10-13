f407ddda493b3a29aecd2131ce1b62a7aadc1a46
zmatej <matej.znidaric@bsse.ethz.ch>
2017-10-13 14:49:33 +0200

    debugging testing2 stimuli;

--- users/zmatej/testing2.py
+++ users/zmatej/testing2.py
@@ -7,24 +7,13 @@ Created on Thu Oct 03 2017
 from visexpman.engine.generic import utils
 from visexpman.engine.vision_experiment import experiment
 
-# -----------------------------------------------------------------------------
-#30 min
-class WhiteNoiseLong(experiment.ExperimentConfig):
-    def _create_parameters(self):
-        self.runnable = 'WhiteNoiseStimulus'
-        self.DURATION_MINS = 30.0 
-        self.PIXEL_SIZE = [40.0]
-        #self.FLICKERING_FREQUENCY = 60.0
-        self.N_WHITE_PIXELS = False
-        self.COLORS = [[0.0, 0.0, 0.0],[0.0, 1.0, 1.0]]
-        self._create_parameters_from_locals(locals())
 
 # -----------------------------------------------------------------------------
 #30 min
 class WhiteNoiseShort(experiment.ExperimentConfig):
     def _create_parameters(self):
         self.runnable = 'WhiteNoiseStimulus'
-        self.DURATION_MINS = 5.0
+        self.DURATION_MINS = 1.0
         self.PIXEL_SIZE = [40.0]
         #self.FLICKERING_FREQUENCY = 60.0
         self.N_WHITE_PIXELS = False
@@ -33,29 +22,10 @@ class WhiteNoiseShort(experiment.ExperimentConfig):
 
 # ------------------------------------------------------------------------------
 #24 min
-class MovingGratingLong(experiment.ExperimentConfig):
-    def _create_parameters(self):
-        self.runnable = 'MovingGratingStimulus'
-        self.REPEATS = 15
-        self.N_BAR_ADVANCES_OVER_POINT = 20
-        self.MARCH_TIME = 0.0
-        self.GREY_INSTEAD_OF_MARCHING = False
-        self.NUMBER_OF_MARCHING_PHASES = 1.0
-        self.GRATING_STAND_TIME = 1.0
-        self.ORIENTATIONS = range(0,360, 45)
-        self.WHITE_BAR_WIDTHS = [100]
-        self.VELOCITIES = [300, 1500]
-        self.DUTY_CYCLES = [1]
-        self.PAUSE_BEFORE_AFTER = 1.0
-        self._create_parameters_from_locals(locals())
-
-
-# ------------------------------------------------------------------------------
-#24 min
 class MovingGratingShort(experiment.ExperimentConfig):
     def _create_parameters(self):
         self.runnable = 'MovingGratingStimulus'
-        self.REPEATS = 3
+        self.REPEATS = 1
         self.N_BAR_ADVANCES_OVER_POINT = 20
         self.MARCH_TIME = 0.0
         self.GREY_INSTEAD_OF_MARCHING = False
@@ -100,7 +70,6 @@ class FullFieldFlashes(experiment.ExperimentConfig):
         self._create_parameters_from_locals(locals())
 
 # -----------------------------------------------------------------------------
-# not working
 class MovingBar(experiment.ExperimentConfig):
     def _create_parameters(self):
         self.runnable = 'MovingShapeStimulus'
@@ -112,21 +81,6 @@ class MovingBar(experiment.ExperimentConfig):
         self.SPEEDS = [300, 1600]  # um/s#
         self.REPETITIONS = 5
         self.SHAPE_BACKGROUND = 0.0
-        self.SHAPE_CONTRAST = 1.0
-        self._create_parameters_from_locals(locals())
-
-class MovingBar2(experiment.ExperimentConfig):
-    def _create_parameters(self):
-        self.runnable = 'MovingGratingStimulus'
-        self.SHAPE_SIZE = utils.cr((1000, 500)) #um
-        #self.SPEEDS = [1600] #um/s#
-        self.SPEEDS = [120, 200,400,1600] #um/s
-        self.PAUSE_BETWEEN_DIRECTIONS = 1.0
-        self.RANDOM_DIRECTIONS = True
-        self.REPETITIONS = 10
-        self.DIRECTIONS = range(0,360,45)
-        self.SHAPE = 'rect'
-        self.SHAPE_BACKGROUND=0.0
         self.SHAPE_CONTRAST = 1.0
         self._create_parameters_from_locals(locals())
 
