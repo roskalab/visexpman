6759013d7d618547d295b86da06c086197fb624f
rz <zoltan.raics@raicszoltan.hu>
2017-10-31 17:10:12 +0100

    play array as audio

--- engine/hardware_interface/sound.py
+++ engine/hardware_interface/sound.py
@@ -1,9 +1,8 @@
 import playsound,subprocess,platform,numpy,scipy.io.wavfile,unittest,tempfile,os,threading,uuid
+import time
 
 class SoundGenerator():
     def __init__(self):
-        if platform.system()!='Windows':
-            raise NotImplementedError()
         self.sample_rate=44100
         self.amplitude=2**15-1
         id=str(uuid.uuid1())
@@ -29,23 +28,65 @@ class SoundGenerator():
                 os.remove(fn)
                 
 class SoundPlayer(threading.Thread):
-    def __init__(self, filename):
-        if os.path.splitext(filename)[1]!='.mp3':
-            raise NotImplementedError()
-        self.filename=filename
+    def __init__(self,filename,sample_rate=None,waveforms=None ):
+        self.waveforms=waveforms
+        if hasattr(waveforms, 'put'):
+            self.s=SoundGenerator()
+            self.s.sample_rate=sample_rate
+        elif hasattr(filename,'dtype'):
+            self.s=SoundGenerator()
+            self.s.sample_rate=sample_rate
+            self.s.array2mp3(filename)
+            self.filename=self.s.mp3fn
+        else:
+            if os.path.splitext(filename)[1]!='.mp3':
+                raise NotImplementedError()
+            self.filename=filename
         threading.Thread.__init__(self)
         
     def run(self):
-        playsound.playsound(self.filename)
+        if hasattr(self.waveforms, 'get'):
+            while True:
+                time.sleep(0.05)
+                if not self.waveforms.empty():
+                    msg=self.waveforms.get()
+                    if msg=='terminate':
+                        break
+                    elif hasattr(msg, 'dtype'):
+                        if os.path.exists(self.s.mp3fn):
+                            os.remove(self.s.mp3fn)
+                        self.s.array2mp3(msg)
+                        playsound.playsound(self.s.mp3fn)
+        else:
+            playsound.playsound(self.filename)
         
 class TestSound(unittest.TestCase):
-    def test_01(self):
+    @unittest.skip('')
+    def test_01_modulated_sound(self):
         s=SoundGenerator()
         s.generate_modulated_sound(1,1000,10)
         p=SoundPlayer(s.mp3fn)
         p.start()
         p.join()
-        pass
+        
+    def test_02_waveform(self):
+        sample_rate=44100
+        waveform=numpy.random.random(40000)
+        import time,Queue
+        wf=Queue.Queue()
+        sp=SoundPlayer(waveform, sample_rate,wf)
+        
+        #playsound.playsound(sp.filename)
+        sp.start()
+        for i in range(3):
+            wf.put(waveform)
+            time.sleep(0.0)
+        wf.put('terminate')
+        sp.join()
+        
+        
+    
+
         
 if __name__ == "__main__":    
     unittest.main()
