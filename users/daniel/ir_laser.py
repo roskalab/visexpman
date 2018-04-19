1690bc3eb99a9263960c97e01bd1f6ab69ce05b7
hd <hd@rlvivo1>
2018-04-19 15:52:33 +0200

    Stimulus modifications automatically saved

--- users/daniel/ir_laser.py
+++ users/daniel/ir_laser.py
@@ -9,6 +9,15 @@ class IRLaser(stimuli.LaserPulseC):
         self.NPULSES=5
         self.LASER_AMPLITUDE=[0.3, 0.5, 0.7, 0.9, 1.1, 1.3] #
 
+class IRLaser980(stimuli.LaserPulseC):
+    def _create_parameters(self):
+        stimuli.LaserPulseC._create_parameters(self)
+        self.INITIAL_DELAY=5 #
+        self.PULSE_DURATION=[100E-3]
+        self.PERIOD_TIME=[10.0]
+        self.NPULSES=5
+        self.LASER_AMPLITUDE=[1.35, 1.5, 1.75, 2.05, 2.25, 2.47] #
+
 class IRLaser1Rep(stimuli.LaserPulseC):
     def _create_parameters(self):
         stimuli.LaserPulseC._create_parameters(self)
