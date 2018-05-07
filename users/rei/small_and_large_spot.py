f17dc65439c384bb7cd6e2670dec6f96888f8c28
Zoltan Raics <zoltan.raics@raicszoltan.hu>
2018-05-07 13:22:57 +0200

    small and large spot stim correction

--- users/rei/small_and_large_spot.py
+++ users/rei/small_and_large_spot.py
@@ -4,8 +4,7 @@ class SmallAndLargeSpotParameters(experiment.ExperimentConfig):
     def _create_parameters(self):
         self.SMALL_SPOT_SIZE=120
         self.LARGE_SPOT_SIZE=3900
-        self.SMALL_SPOT_TIME=10
-        self.LARGE_SPOT_TIME=2
+        self.SPOT_TIME=2
         self.INITIAL_WAIT=10
         self.POST_WAIT=5
         self.runnable='SmallAndLargeSpotE'
@@ -14,10 +13,9 @@ class SmallAndLargeSpotParameters(experiment.ExperimentConfig):
 class SmallAndLargeSpotE(experiment.Experiment):
     def run(self):
         ec=self.experiment_config
-        self.show_fullscreen(color=0.0, duration=ec.INITIAL_WAIT)
-        self.show_shape(color=1.0, size=ec.SMALL_SPOT_SIZE, duration=ec.SMALL_SPOT_TIME)
-        self.show_shape(color=1.0, size=ec.LARGE_SPOT_SIZE, duration=ec.LARGE_SPOT_TIME)
-        self.show_fullscreen(color=0.0, duration=ec.POST_WAIT)
+        self.show_shape(color=1.0, size=ec.SMALL_SPOT_SIZE, duration=ec.INITIAL_WAIT)
+        self.show_shape(color=1.0, size=ec.LARGE_SPOT_SIZE, duration=ec.SPOT_TIME)
+        self.show_shape(color=1.0, size=ec.SMALL_SPOT_SIZE, duration=ec.POST_WAIT)
         
 if __name__ == "__main__":
     from visexpman.applications.visexp_app import stimulation_tester
