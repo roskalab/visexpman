b0791897bf3136eb8d7143bcbcb90552bedb66f9
Zoltan Raics <zoltan.raics@raicszoltan.hu>
2018-06-22 17:44:31 +0200

    semi-automated bouton detection (not ready)

--- engine/analysis/bouton.py
+++ engine/analysis/bouton.py
@@ -2,16 +2,19 @@ from skimage.feature import register_translation
 from PIL import Image
 import numpy,os,unittest
 from visexpman.engine.generic import fileop
+from visexpman.engine.vision_experiment import experiment_data
             
 def motion_correction(images):
     corrected=numpy.copy(images)
     for i in range(images.shape[0]):
         if i>0:
             res= register_translation(images[0], images[i])[0]
+            #print((res[0], res[1]))
             corrected[i]=numpy.roll(numpy.roll(images[i],int(res[0]), axis=0),int(res[1]),axis=1)
     return corrected
-
+    
 class Test(unittest.TestCase):
+    @unittest.skip("")
     def test_01_motion_correction(self):
         from pylab import plot,show,legend,ylim
         folders=['More smear/716-18d-vivo-m1-reg3-rep1-LGN-2levels',
@@ -42,6 +45,35 @@ class Test(unittest.TestCase):
         ylim([-10,10])
         legend(['no smear x', 'no smear y', 'little smear x', 'little smear y', 'more smear x', 'more smear y'])
         show()
+        
+    def test_02_detect_cell(self):
+        for f in fileop.listdir('/tmp'):
+            if f[-5:]!='.hdf5':continue
+            print f
+            h=experiment_data.CaImagingData(f)
+            h.load('raw_data')
+            before=numpy.copy(h.get_image(image_type='mean')[0])
+            h.raw_data[:,0]=motion_correction(h.raw_data[:,0])
+            after=h.get_image(image_type='mip')[0]
+            from pylab import *
+#            figure(1);imshow(h.raw_data[:,0].mean(axis=0));figure(2);imshow(corrected.mean(axis=0));show()
+            #figure(1);imshow(before);
+            #figure(2);imshow(after);
+            from skimage import filters
+            thr=filters.threshold_otsu(after)
+            bw=numpy.where(after>thr,1,0)
+            import scipy.ndimage.measurements
+            labeled, n = scipy.ndimage.measurements.label(bw)
+            import skimage.segmentation, scipy.ndimage.morphology
+            labeled=skimage.segmentation.clear_border(labeled)
+            labeled=scipy.ndimage.morphology.binary_fill_holes(labeled)
+            
+            figure(1);imshow(after)
+            figure(2);imshow(labeled)
+            
+            
+            show()
+            h.close()
         
 if __name__ == "__main__":
     unittest.main()
