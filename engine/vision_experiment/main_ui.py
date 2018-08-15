e976fea950bf0044d62d06cb41db771c3efe0f68
Zoltan Raics <zoltan.raics@raicszoltan.hu>
2018-08-15 22:37:04 +0200

    visexpman version displayed in statusbar

--- engine/vision_experiment/main_ui.py
+++ engine/vision_experiment/main_ui.py
@@ -655,7 +655,8 @@ class MainUI(gui.VisexpmanMainWindow):
             elif 'display_cell_tree' in msg:
                 self.cellbrowser.populate(msg['display_cell_tree'])
             elif 'update_network_status' in msg:
-                self.statusbar.showMessage(msg['update_network_status'])
+                import visexpman
+                self.statusbar.showMessage(msg['update_network_status']+20*' '+'visexpman '+visexpman.version)
             elif 'update_status' in msg:
                 if msg['update_status']=='idle':
                     self.statusbar.status.setStyleSheet('background:gray;')
