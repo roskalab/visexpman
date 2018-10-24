4cff316c610bf6a635bce0dc4e7521249c5e3b25
hd <hd@rlvivo1>
2018-10-24 17:58:11 +0200

    plotting: blockplot_mpl refactored for grouped keyword arguments (e.g. arrow_kw) datatypes.quantify_directions: badly defined stimulus length caused too short twophoton data recording. This means the last block of a recording can be very short. Added case for checking if last block was shorter than mean block length.
    importers.mesc2visexpa refactored into a class.
    importers: changed regexp strings to raw strings (py3 compatibility)
    
    legacy: collects all files belonging to the specified experiments into one long list
    
    configuration: created new ini file based command-line argument parsing. These ini files should contain parameters needed to create figures for Dasha paper

--- users/daniel/ir_laser.py
+++ users/daniel/ir_laser.py
@@ -23,6 +23,16 @@ class IRLaser(stimuli.LaserPulseC):
         self.NPULSES=5
         self.LASER_AMPLITUDE=[0.3, 0.5, 0.7, 0.9, 1.1, 1.3] #
 
+
+class IRLaser755(stimuli.LaserPulseC):
+    def _create_parameters(self):
+        stimuli.LaserPulseC._create_parameters(self)
+        self.INITIAL_DELAY=5 #
+        self.PULSE_DURATION=[100E-3]
+        self.PERIOD_TIME=[10.0]
+        self.NPULSES=5
+        self.LASER_AMPLITUDE=[0.2, 0.3, 0.5, 0.7, 0.9, 1.1, 1.3] #
+
 class IRLaser980(stimuli.LaserPulseC):
     def _create_parameters(self):
         stimuli.LaserPulseC._create_parameters(self)
