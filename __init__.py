65f4ad59876ef32518e9c5c88e6c02e0ecb5d05c
Zoltan Raics <zoltan.raics@raicszoltan.hu>
2019-01-24 11:01:35 +0100

    get user module folder modified

--- __init__.py
+++ __init__.py
@@ -6,7 +6,7 @@ import numpy
 
 version = 'v0.4.0'
 if '--vu' in sys.argv:
-    USER_MODULE= sys.argv[sys.argv.index('--vu')+1]+".users"
+    USER_MODULE= str(sys.argv[sys.argv.index('--vu')+1]+".users")
 else:
     USER_MODULE='visexpman.users'
 try:
