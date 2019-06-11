0e7fd2f8423b84a8a2d95c4d615137a45bd8ca84
hd <hd@rlvivo1>
2019-06-11 08:01:35 +0200

    added Thorlabs power measurements for IRLaser980high experiments align.gather_overlaping_rois: still looks for overlapping CNMF rois but always creates simple df/f curve at ROI location translated from ref ROI. align.population_plotting: now computes pvalues between first 1500 ms of ref and offset ROI. roi.extract_time_interval_from_each_block: extracts samples covering X ms from each block

--- users/daniel/ir_laser.py
+++ users/daniel/ir_laser.py
@@ -60,6 +60,33 @@ class IRLaser980b(stimuli.LaserPulseC):
         self.NPULSES=5
         self.LASER_AMPLITUDE=[1.75, 2.05, 2.25, 2.47, 2.6, 2.8] #
 
+class IRLaser910high(stimuli.LaserPulseC):
+    def _create_parameters(self):
+        stimuli.LaserPulseC._create_parameters(self)
+        self.INITIAL_DELAY=5 #
+        self.PULSE_DURATION=[100E-3]
+        self.PERIOD_TIME=[10.0]
+        self.NPULSES=5
+        self.LASER_AMPLITUDE=[1.23, 1.365] #
+
+class IRLaser980high(stimuli.LaserPulseC):
+    def _create_parameters(self):
+        stimuli.LaserPulseC._create_parameters(self)
+        self.INITIAL_DELAY=5 #
+        self.PULSE_DURATION=[100E-3]
+        self.PERIOD_TIME=[10.0]
+        self.NPULSES=5
+        self.LASER_AMPLITUDE=[2.6, 2.8] #
+
+class IRLaser980highwait(stimuli.LaserPulseC):
+    def _create_parameters(self):
+        stimuli.LaserPulseC._create_parameters(self)
+        self.INITIAL_DELAY=5 #
+        self.PULSE_DURATION=[100E-3]
+        self.PERIOD_TIME=[30.0]
+        self.NPULSES=5
+        self.LASER_AMPLITUDE=[2.6, 2.8] #
+
 class IRLaser1Rep(stimuli.LaserPulseC):
     def _create_parameters(self):
         stimuli.LaserPulseC._create_parameters(self)
@@ -76,7 +103,10 @@ class IRLaserCal910(stimuli.LaserPulseC):
         self.PULSE_DURATION=[5]
         self.PERIOD_TIME=[10.0]
         self.NPULSES=1
-        self.LASER_AMPLITUDE=[1.1, 1.3] #
+        self.LASER_AMPLITUDE=[0.5, 0.7, 0.9, 1.1, 1.3, 1.4] #
+        self.LASER_AMPLITUDE=[1.23]
+        # Measured with ND2 filter and thorlabs s170c:
+        # 0.008, 0.3, 0.5, 7.3, 14.9, 22.7
 
 class IRLaserCal980(stimuli.LaserPulseC):
     def _create_parameters(self):
@@ -85,4 +115,9 @@ class IRLaserCal980(stimuli.LaserPulseC):
         self.PULSE_DURATION=[5]
         self.PERIOD_TIME=[10.0]
         self.NPULSES=1
-        self.LASER_AMPLITUDE=[2.25, 2.47] #
+        self.LASER_AMPLITUDE=[2.8]#, 2.6, 2.8] #
+#	self.LASER_AMPLITUDE=[1.75] #
+        # Measured with ND2 fitler and thorlabd s170c:
+        #                    0.04, 0.09, 4.5, 11.6, 19.4, 23.8, 27.2, 30.5
+        # from:              0.03, 0.08, 2.8, 9.8, 16.9, 21.8, 24.1, 28.7
+
