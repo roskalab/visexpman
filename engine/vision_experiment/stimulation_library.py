e2ce78bae9a78a9df32abf811525c79aa978759f
Zoltan Raics <zoltan.raics@raicszoltan.hu>
2019-07-23 17:21:03 +0200

    video file duration reader

--- engine/vision_experiment/stimulation_library.py
+++ engine/vision_experiment/stimulation_library.py
@@ -375,7 +375,7 @@ class Stimulations(experiment_control.StimulationControlHelper):#, screen.Screen
                 
     def show_video(self, fn, position = (0, 0),  stretch=1.0):
         import skvideo.io
-        skvideo.setFFmpegPath(fileop.visexpman_package_path())#ffmpeg.exe and ffprobe shall be located here
+        skvideo.setFFmpegPath(os.path.dirname(fileop.visexpman_package_path()))#ffmpeg.exe and ffprobe shall be located here
         self.video = numpy.cast['float'](skvideo.io.vread(fname = fn))/255.
         for i in range(self.video.shape[0]):
             if self.machine_config.ENABLE_TIME_INDEXING:
@@ -385,6 +385,12 @@ class Stimulations(experiment_control.StimulationControlHelper):#, screen.Screen
             self._show_image(self.video[index],0,utils.cr((position[0], position[1])),stretch, True)
             if self.abort:
                 break
+                
+    def get_video_duration(self, fn):
+        import skvideo.io
+        skvideo.setFFmpegPath(os.path.dirname(fileop.visexpman_package_path()))#ffmpeg.exe and ffprobe shall be located here
+        self.video = numpy.cast['float'](skvideo.io.vread(fname = fn))/255.
+        return float(self.video.shape[0])/self.machine_config.SCREEN_EXPECTED_FRAME_RATE
 
     def show_shape(self, shape = '',  duration = 0.0,  pos = utils.rc((0,  0)),  color = [1.0,  1.0,  1.0],  
                 background_color = None,  orientation = 0.0,  size = utils.rc((0,  0)),  ring_size = None, 
