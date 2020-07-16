8020c96028328c90fa20c37db9faf3cb521e059e
Zoltan Raics <zoltan@raics.hu>
2020-07-16 12:54:01 +0200

    Video splitter python3 compatible

--- applications/video_splitter.py
+++ applications/video_splitter.py
@@ -9,10 +9,11 @@ def video_splitter():
         gui.message('Error', 'Invalid duration: {0}'.format(chunk_duration))
         return
     try:
+        print(' '.join(['ffmpeg','-i', '{0}'.format(filename), '-vcodec', 'copy', '-an', '-f', 'null', '-']))
         p=subprocess.Popen(['ffmpeg','-i', '{0}'.format(filename), '-vcodec', 'copy', '-an', '-f', 'null', '-'],stderr=subprocess.PIPE)
-        res=p.stderr.read()
+        res=p.stderr.read().decode('utf-8')
         durstr=res.split('Duration: ')[1].split(', ')[0]
-        overall_duration=(numpy.array(map(float, durstr.split(':')))*numpy.array([3600,60,1])).sum()
+        overall_duration=(numpy.array(list(map(float, durstr.split(':'))))*numpy.array([3600,60,1])).sum()
         chunk_duration*=60
         nchunks=int(numpy.ceil(overall_duration/chunk_duration))
         for i in range(nchunks):
@@ -20,6 +21,8 @@ def video_splitter():
             subprocess.call('ffmpeg -i "{0}" -ss {1} -t {2} -c copy "{3}"'.format(filename, i*chunk_duration, chunk_duration, outputfn),shell=True)
         gui.message('Info', 'Done')
     except:
+        import traceback
+        print(traceback.format_exc())
         import pdb
         pdb.set_trace()
 
