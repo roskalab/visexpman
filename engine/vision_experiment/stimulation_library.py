cbd14767f93235b26ac24b92fc20623420f4a929
Nemeth Arpad <n.arpi24@gmail.com>
2020-11-06 16:08:26 +0100

    debug mode

--- engine/vision_experiment/stimulation_library.py
+++ engine/vision_experiment/stimulation_library.py
@@ -1843,20 +1843,22 @@ class Stimulations(experiment_control.StimulationControlHelper):#, screen.Screen
             [self.machine_config.SCREEN_WIDTH / 2 + self.DISPLACEMENT_FROM_SCREEN_CENTER[0] * -1,
              self.machine_config.SCREEN_HEIGHT / 2 + self.DISPLACEMENT_FROM_SCREEN_CENTER[1] * -1]) / (
                                                                    self.machine_config.SCREEN_DISTANCE_FROM_MOUSE_EYE * 0.1)))
-
-        print('start angle', self.screen_start_angle)
-        print('end_angle', self.screen_end_angle)
+        if self.DEBUG_MODE:
+            print('start angle', self.screen_start_angle)
+            print('end_angle', self.screen_end_angle)
         #
         colum_coords = numpy.arange(self.screen_start_angle[1]+(grid_size_deg/2), self.screen_end_angle[1], grid_size_deg)
         row_coords = numpy.arange(self.screen_start_angle[0]+(grid_size_deg/2), self.screen_end_angle[0], grid_size_deg)
-        print('row coords', row_coords)
-        print('lenrow',len(row_coords))
-        print('len_col',len(colum_coords))
-        print('colun coords', colum_coords)
+        if self.DEBUG_MODE:
+            print('row coords', row_coords)
+            print('lenrow',len(row_coords))
+            print('len_col',len(colum_coords))
+            print('colun coords', colum_coords)
         grid = numpy.meshgrid(row_coords, colum_coords)
         # positions = list(zip(*(x.flat for x in grid)))
         positions = numpy.vstack(map(numpy.ravel, grid)).T
-        print(positions.shape)
+        if self.DEBUG_MODE:
+            print(positions.shape)
         # positions = numpy.asarray(positions)
         # for i in range(len(positions)):
         #     print(positions[i])
@@ -1885,9 +1887,10 @@ class Stimulations(experiment_control.StimulationControlHelper):#, screen.Screen
         #                          0)
         # positions=numpy.flip(positions, axis=1)
         positions = numpy.round(positions, decimals=2)
-        print(positions.shape)
-        for i in range(len(positions)):
-            print(positions[i])
+        if self.DEBUG_MODE:
+            print(positions.shape)
+            for i in range(len(positions)):
+                print(positions[i])
         return positions
 
     def angle2pixel(self, xy):
